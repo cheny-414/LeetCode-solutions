9c883b5631545e38a3a6072c88a0ebd655407c3c
cheny-414 <94153287+cheny-414@users.noreply.github.com>
2023-06-11 20:00:22 -0400

    Time: 1 ms (100.00%) | Memory: 44.5 MB (6.94%) - LeetSync

---
+++ find-pivot-index/find-pivot-index.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int pivotIndex(int[] nums) {
+        int sum = 0;
+        for (int num : nums) {
+            sum += num;
+        }
+        int left = 0;
+
+       for (int i = 0; i < nums.length; i++) {
+            if (left * 2 == sum - nums[i]) return i;
+            left += nums[i];
+        }
+        return -1;
+    }
+}
