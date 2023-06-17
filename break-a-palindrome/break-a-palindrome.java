932e0ce3f327cf4f772cb972162a9f10214ed24f
cheny-414 <94153287+cheny-414@users.noreply.github.com>
2023-06-17 18:59:45 -0400

    Time: 0 ms (100.00%) | Memory: 40.7 MB (71.53%) - LeetSync

---
+++ break-a-palindrome/break-a-palindrome.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String breakPalindrome(String palindrome) {
+        int length = palindrome.length();
+        StringBuilder sb = new StringBuilder();
+        if (length <= 1) return "";
+        int mid = -1;
+        if (length % 2 == 1) mid = (length / 2);
+        for (int i = 0; i < length; i++) {
+            if (palindrome.charAt(i) - 'a' > 0 && (mid == -1 || (mid != -1 && i != mid))) {
+                String newString = palindrome.substring(0, i) + "a";
+                if (i < length - 1) {
+                    newString += palindrome.substring(i+1);
+                }
+                return newString;
+            }
+        }
+        return palindrome.substring(0, length - 1) + 'b';
+    }
+}
