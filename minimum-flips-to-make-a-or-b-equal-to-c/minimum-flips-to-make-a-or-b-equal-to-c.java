7effa349d3ec837289eea6032364b55756fff66b
cheny-414 <94153287+cheny-414@users.noreply.github.com>
2023-06-20 18:55:05 -0400

    Time: 0 ms (100.00%) | Memory: 38.7 MB (98.91%) - LeetSync

--- minimum-flips-to-make-a-or-b-equal-to-c/minimum-flips-to-make-a-or-b-equal-to-c.java
+++ minimum-flips-to-make-a-or-b-equal-to-c/minimum-flips-to-make-a-or-b-equal-to-c.java
@@ -1,19 +1,8 @@
 class Solution {
     public int minFlips(int a, int b, int c) {
         int answer = 0;
-        while (a != 0 | b != 0 | c != 0) {
-            if ((c & 1) == 1) {
-                if ((a & 1) == 0 && (b & 1) == 0) {
-                    answer++;
-                }
-            } else if ((c & 1) == 0) {
-                answer += (a & 1);
-                answer += (b & 1);
-            }
-            a >>= 1;
-            b >>= 1;
-            c >>= 1;
-        }
-        return answer;
+        int result1 = (a | b) ^ c;
+        int result2 = (a & b) & result1;
+        return Integer.bitCount(result1) + Integer.bitCount(result2);
     }
 }
