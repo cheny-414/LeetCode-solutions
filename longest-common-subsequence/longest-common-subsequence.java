ecbf7d8e8437c0ef3842a5abb50ea25bb2445848
cheny-414 <94153287+cheny-414@users.noreply.github.com>
2023-06-19 23:05:18 -0400

    Time: 52 ms (8.01%) | Memory: 48.8 MB (49.61%) - LeetSync

---
+++ longest-common-subsequence/longest-common-subsequence.java
@@ -0,0 +1,38 @@
+class Solution {
+    int[][] memo;
+    String text1;
+    String text2;
+    public int longestCommonSubsequence(String text1, String text2) {
+        memo = new int[text1.length()][text2.length()];
+        this.text1 = text1;
+        this.text2 = text2;
+        for (int i = 0; i < text1.length(); i++) {
+            for (int j = 0; j < text2.length(); j++) {
+                memo[i][j] = -1;
+            }
+        }
+        return solve(0,0);
+    }
+
+    private int solve(int p1, int p2) {
+        if (p1 == text1.length() - 1) {
+            int occur = text2.indexOf(text1.charAt(p1), p2);
+            return occur == -1 ? 0 : 1;
+        }
+        if (p2 == text2.length() - 1) {
+            int occur = text1.indexOf(text2.charAt(p2), p1);
+            return occur == -1 ? 0 : 1;
+        }
+        if (memo[p1][p2] != -1) {
+            return memo[p1][p2];
+        }
+        int answer = 0;
+        if (text1.charAt(p1) == text2.charAt(p2)) {
+            answer = 1 + solve(p1 + 1, p2 + 1);
+        } else {
+            answer = Math.max(solve(p1, p2 + 1), solve(p1 + 1, p2));
+        }
+        memo[p1][p2] = answer;
+        return answer;
+    }
+}
