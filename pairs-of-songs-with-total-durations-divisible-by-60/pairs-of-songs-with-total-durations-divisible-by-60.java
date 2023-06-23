91490dc53aed2e92a7cdf409bbea3658e9f3c720
cheny-414 <94153287+cheny-414@users.noreply.github.com>
2023-06-22 23:58:39 -0400

    Time: 3 ms (85.03%) | Memory: 53.2 MB (22.76%) - LeetSync

--- pairs-of-songs-with-total-durations-divisible-by-60/pairs-of-songs-with-total-durations-divisible-by-60.java
+++ pairs-of-songs-with-total-durations-divisible-by-60/pairs-of-songs-with-total-durations-divisible-by-60.java
@@ -1,20 +1,16 @@
 class Solution {
     public int numPairsDivisibleBy60(int[] time) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        HashSet<Integer> seen = new HashSet<>();
-        int pairs = 0;
-        for (int song : time) {
-            map.put(song % 60, map.getOrDefault(song % 60, 0) + 1);
-        }
+        int[] remainders = new int[60];
+        int answer = 0;
 
-        pairs += map.getOrDefault(30, 0) * (map.getOrDefault(30, 0) - 1) / 2;
-        pairs += (long)map.getOrDefault(0, 0) * (map.getOrDefault(0, 0) - 1) / 2;
-        for (int i : map.keySet()) {
-            if (i != 30 && i != 0 && !seen.contains(i)) {
-                pairs += map.getOrDefault(i, 0) * map.getOrDefault(60-i, 0);
-                seen.add(60-i);
+        for (int t : time) {
+            if (t % 60 == 0) {
+                answer += remainders[0]++;
+            } else {
+                answer += remainders[60 - t % 60];
+                remainders[t % 60]++;
             }
         }
-        return pairs;
+        return answer;
     }
 }
