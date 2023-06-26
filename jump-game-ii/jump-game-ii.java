a4be25d306939e245ca7615910298ace2012e6a2
cheny-414 <94153287+cheny-414@users.noreply.github.com>
2023-06-25 23:01:03 -0400

    Time: 1 ms (99.38%) | Memory: 44.7 MB (36.84%) - LeetSync

---
+++ jump-game-ii/jump-game-ii.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int jump(int[] nums) {
+        if (nums.length == 1) return 0;
+        int reach = nums[0];
+        int jumps = 0;
+        int index = 0;
+        while (reach < nums.length - 1) {
+            int max = index;
+            int jumpTo = index;
+            for (int i = index + 1; i <= reach; i++) {
+                if (i + nums[i] > max) {
+                    max = i + nums[i];
+                    jumpTo = i;
+                }
+            }
+            index = jumpTo;
+            jumps++;
+            reach = index + nums[index];
+        }
+        return jumps + 1;
+    }
+}
