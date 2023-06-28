2a0d0bff74b9364ab00ea3a3c9d9df8e24fe980f
cheny-414 <94153287+cheny-414@users.noreply.github.com>
2023-06-28 14:53:37 -0400

    Time: 12 ms (80.76%) | Memory: 44.8 MB (17.41%) - LeetSync

--- substring-with-concatenation-of-all-words/substring-with-concatenation-of-all-words.java
+++ substring-with-concatenation-of-all-words/substring-with-concatenation-of-all-words.java
@@ -1,41 +1,54 @@
 class Solution {
     public List<Integer> findSubstring(String s, String[] words) {
-        int wordCount = words.length;
-        int wordLen = words[0].length();
-        int n = s.length();
+        List<Integer> answer = new ArrayList<>();
         Map<String, Integer> map = new HashMap<>();
-        List<Integer> result = new ArrayList<>();
+        Map<String, Integer> map2 = new HashMap<>();
+        int wordLength = words[0].length();
 
         for (String word : words) {
             map.put(word, map.getOrDefault(word, 0) + 1);
+            map2.put(word, map2.getOrDefault(word, 0) + 1);
         }
 
-        for (int i = 0; i < wordLen; i++) {
-            int wordsLeft = wordCount;
-            Map<String, Integer> seen = new HashMap<>();
 
-            for (int right = i, left = i; right <= n - wordLen; right += wordLen) {
-                String target = s.substring(right, right + wordLen);
-                if (!map.containsKey(target)) {
-                    seen.clear();
-                    wordsLeft = wordCount;
-                    left = right + wordLen;
-                } else {
-                    seen.put(target, seen.getOrDefault(target, 0) + 1);
-                    while (seen.get(target) > map.get(target)) {
-                        String removeWord = s.substring(left, left + wordLen);
-                        seen.put(removeWord, seen.get(removeWord) - 1);
-                        left += wordLen;
-                        ++wordsLeft;
+        for (int i = 0; i < wordLength; i++) {
+            for (String key : map2.keySet()) {
+                map.put(key, map2.get(key));
+            }
+            int left = i;
+            int right = i + wordLength;
+            while (right <= s.length()) {
+                String wordCheck = s.substring(right - wordLength, right);
+                if (map.containsKey(wordCheck)) {
+                    if (map.get(wordCheck) > 0) {
+                        map.put(wordCheck, map.get(wordCheck) - 1);
+                        if (right - left == wordLength * words.length) {
+                            if (!answer.contains(left)) {
+                                answer.add(left);
+                            }
+                            String wordRelease = s.substring(left, left + wordLength);
+                            map.put(wordRelease, map.get(wordRelease) + 1);
+                            left += wordLength;
+                        }
+                    } else {
+                        while (map.get(wordCheck) == 0) {
+                            String wordRelease = s.substring(left, left + wordLength);
+                            map.put(wordRelease, map.get(wordRelease) + 1);
+                            left += wordLength;
+                        }
+                        map.put(wordCheck, map.get(wordCheck) - 1);
                     }
-                    --wordsLeft;
+                } else {
+                    while (left < right - wordLength) {
+                        String wordRelease = s.substring(left, left + wordLength);
+                        map.put(wordRelease, map.get(wordRelease) + 1);
+                        left += wordLength;
+                    }
+                    left = right;
                 }
-
-                if (wordsLeft == 0) {
-                    result.add(left);
-                }
+                right += wordLength;
             }
         }
-        return result;
+        return answer;
     }
 }
