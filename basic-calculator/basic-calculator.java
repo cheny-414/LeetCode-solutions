5b83c59f06487f0c8eac35390084946df378f3e3
cheny-414 <94153287+cheny-414@users.noreply.github.com>
2023-06-29 14:41:18 -0400

    Time: 14 ms (32.06%) | Memory: 43.7 MB (73.86%) - LeetSync

--- basic-calculator/basic-calculator.java
+++ basic-calculator/basic-calculator.java
@@ -2,6 +2,7 @@ class Solution {
     public int calculate(String s) {
         Stack<Integer> stack = new Stack<Integer>();
         int result = 0;
+        int number = 0;
         int sign = 1;
         for(int i = 0; i < s.length(); i++){
             if (Character.isDigit(s.charAt(i))) {
