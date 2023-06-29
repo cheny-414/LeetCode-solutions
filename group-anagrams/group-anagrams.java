66b400ff77e4d6da07535ce7c1a378f3f5dc4275
cheny-414 <94153287+cheny-414@users.noreply.github.com>
2023-06-29 01:05:38 -0400

    Time: 6 ms (98.18%) | Memory: 47 MB (45.76%) - LeetSync

---
+++ group-anagrams/group-anagrams.java
@@ -0,0 +1,19 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<String, List<String>> map = new HashMap<>();
+        for (String str : strs) {
+            String key = getKey(str);
+            if (!map.containsKey(key)) {
+                map.put(key, new ArrayList());
+            }
+            map.get(key).add(str);
+        }
+        return new ArrayList(map.values());
+    }
+
+    private String getKey(String str) {
+        char[] strArr = str.toCharArray();
+        Arrays.sort(strArr);
+        return new String(strArr);
+    }
+}
