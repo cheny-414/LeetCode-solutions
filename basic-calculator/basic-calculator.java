95bdcdfc0cdbb5f75ecdf6f13027799722d56387
cheny-414 <94153287+cheny-414@users.noreply.github.com>
2023-06-29 14:40:49 -0400

    Time: 14 ms (32.06%) | Memory: 43.7 MB (73.86%) - LeetSync

--- basic-calculator/basic-calculator.java
+++ basic-calculator/basic-calculator.java
@@ -5,32 +5,29 @@ class Solution {
         int number = 0;
         int sign = 1;
         for(int i = 0; i < s.length(); i++){
-            char c = s.charAt(i);
-            if(Character.isDigit(c)){
-                number = 10 * number + (int)(c - '0');
-            }else if(c == '+'){
-                result += sign * number;
-                number = 0;
+            if (Character.isDigit(s.charAt(i))) {
+                int sum = s.charAt(i) - '0';
+                while (i + 1 < s.length() && Character.isDigit(s.charAt(i + 1))) {
+                    sum = sum * 10 + s.charAt(i + 1) - '0';
+                    i++;
+                }
+			    result += sum * sign;
+            } else if(s.charAt(i) == '+'){
                 sign = 1;
-            }else if(c == '-'){
-                result += sign * number;
-                number = 0;
+            } else if(s.charAt(i) == '-'){
                 sign = -1;
-            }else if(c == '('){
+            } else if(s.charAt(i) == '('){
                 //we push the result first, then sign;
                 stack.push(result);
                 stack.push(sign);
                 //reset the sign and result for the value in the parenthesis
                 sign = 1;   
                 result = 0;
-            }else if(c == ')'){
-                result += sign * number;  
-                number = 0;
+            } else if(s.charAt(i) == ')'){
                 result *= stack.pop();    //stack.pop() is the sign before the parenthesis
                 result += stack.pop();   //stack.pop() now is the result calculated before the parenthesis
             }
         }
-        if(number != 0) result += sign * number;
         return result;
     }
 }
