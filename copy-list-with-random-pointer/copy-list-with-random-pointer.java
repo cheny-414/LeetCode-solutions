0c6c2e3e262124b2f7061a2f6fed66abc57149dd
cheny-414 <94153287+cheny-414@users.noreply.github.com>
2023-06-29 16:37:56 -0400

    Time: 0 ms (100.00%) | Memory: 43.8 MB (10.08%) - LeetSync

---
+++ copy-list-with-random-pointer/copy-list-with-random-pointer.java
@@ -0,0 +1,34 @@
+/*
+// Definition for a Node.
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
+*/
+
+class Solution {
+    HashMap<Node, Node> visitedHash = new HashMap<Node, Node>();
+
+    public Node copyRandomList(Node head) {
+        if (head == null) return null;
+
+        if (visitedHash.containsKey(head)) {
+            return visitedHash.get(head);
+        }
+
+        Node node = new Node(head.val);
+
+        visitedHash.put(head, node);
+        node.next = copyRandomList(head.next);
+        node.random = copyRandomList(head.random);
+
+        return node;
+    }
+}
