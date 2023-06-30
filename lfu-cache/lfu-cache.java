65fe0a052de73e61a5f856ca0e69e9de28938cfd
cheny-414 <94153287+cheny-414@users.noreply.github.com>
2023-06-30 15:41:43 -0400

    Time: 89 ms (31.53%) | Memory: 127.5 MB (33.15%) - LeetSync

--- lfu-cache/lfu-cache.java
+++ lfu-cache/lfu-cache.java
@@ -15,43 +15,43 @@ class LFUCache {
         int freq = cache.get(key).getKey();
         int value = cache.get(key).getValue();
         Set<Integer> keys = freqs.get(freq);
-        keys.remove(key);
+        keys.remove(key);                   //remove key from hashset
         if (keys.isEmpty()) {
             freqs.remove(freq);
             if (minFreq == freq) {
                 minFreq++;
             }
         }
-        insert(key, freq + 1, value);
+        insert(key, freq + 1, value);       //re-insert key to hashset, with freq + 1
         return value;
     }
     
     public void put(int key, int value) {
         if (capacity <= 0) return;
-        if (cache.containsKey(key)) {
-            cache.put(key, new Pair<>(cache.get(key).getKey(), value));
-            get(key);
+        if (cache.containsKey(key)) {                                   //if key exists in cache
+            cache.put(key, new Pair<>(cache.get(key).getKey(), value)); //update cache with new value
+            get(key);                                                   //perform get = remove + re-insert (freq + 1)
             return;
         }
 
-        if (capacity == cache.size()) {
-            Set<Integer> keys = freqs.get(minFreq);
-            int keyToDelete = keys.iterator().next();
-            cache.remove(keyToDelete);
-            keys.remove(keyToDelete);
-            if (keys.isEmpty()) {
+        if (capacity == cache.size()) {                                 //if capacity reached before adding this new key
+            Set<Integer> keys = freqs.get(minFreq);                     //find keys with min frequency
+            int keyToDelete = keys.iterator().next();                   //find first key in that set
+            cache.remove(keyToDelete);                                  //delete key from cache
+            keys.remove(keyToDelete);                                   //delete key from set
+            if (keys.isEmpty()) {                                       //min freq list is empty, delete this freq from map
                 freqs.remove(minFreq);
             }
         }
 
-        minFreq = 1;
-        insert(key, 1, value);
+        minFreq = 1;                                                    //added a brand new key, min freq is now 1
+        insert(key, 1, value);                                          //insert this new key
     }
 
     private void insert(int key, int freq, int value) {
-        cache.put(key, new Pair<>(freq, value));
+        cache.put(key, new Pair<>(freq, value));                        //add key, freq, value to cache
         freqs.putIfAbsent(freq, new LinkedHashSet<>());
-        freqs.get(freq).add(key);
+        freqs.get(freq).add(key);                                       //add key to freq map 
     }
 }
 
