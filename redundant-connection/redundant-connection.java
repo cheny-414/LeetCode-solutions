2044b9d6b1e52860a00cd5b246009ae20d5bcb40
cheny-414 <94153287+cheny-414@users.noreply.github.com>
2023-07-07 11:57:23 -0400

    Time: 0 ms (100.00%) | Memory: 43.7 MB (36.34%) - LeetSync

---
+++ redundant-connection/redundant-connection.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int[] findRedundantConnection(int[][] edges) {
+        int n = edges.length;
+        UnionFind uf = new UnionFind(n + 1);
+        for (int[] e : edges) {
+            if (!uf.union(e[0], e[1])) return e;
+        }
+        return new int[]{};
+    }
+}
+
+class UnionFind {
+    int[] parent, size;
+    public UnionFind(int n) {
+        parent = new int[n]; size = new int[n];
+        for (int i = 1; i < n; i++) {
+            parent[i] = i; size[i] = 1;
+        }
+    }
+
+    public int find(int x) {
+        if (x == parent[x]) return x;
+        return parent[x] = find(parent[x]);
+    }
+
+    public boolean union(int u, int v) {
+        int parentU = find(u), parentV = find(v);
+        if (parentU == parentV) return false;
+        if (size[parentU] > size[parentV]) {
+            size[parentU] += size[parentV];
+            parent[parentV] = parentU;
+        } else {
+            size[parentV] += size[parentU];
+            parent[parentU] = parentV;
+        }
+        return true;
+    }
+}
