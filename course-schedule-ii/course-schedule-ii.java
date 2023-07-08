85db348ed3b713ec5be1e0ea7df83b3ef94e2b44
cheny-414 <94153287+cheny-414@users.noreply.github.com>
2023-07-07 23:42:43 -0400

    Time: 5 ms (84.04%) | Memory: 45.1 MB (14.24%) - LeetSync

---
+++ course-schedule-ii/course-schedule-ii.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        List<List<Integer>> adj = new ArrayList<>(numCourses);
+        int[] indegree = new int[numCourses];
+        int[] res = new int[numCourses];
+
+        for (int i = 0; i < numCourses; i++) {
+            adj.add(new ArrayList<>());
+        }
+
+        for (int[] prereq : prerequisites) {
+            adj.get(prereq[1]).add(prereq[0]);
+            indegree[prereq[0]]++;
+        }
+
+        Queue<Integer> q = new ArrayDeque<>();
+        int nodesVisited = 0;
+
+        for (int i = 0; i < numCourses; i++) {
+            if (indegree[i] == 0) {
+                q.add(i);
+            }
+        }
+
+        while (!q.isEmpty()) {
+            int curr = q.poll();
+            nodesVisited++;
+            res[nodesVisited - 1] = curr;
+            for (int neighbor : adj.get(curr)) {
+                indegree[neighbor]--;
+                if (indegree[neighbor] == 0) {
+                    q.add(neighbor);
+                }
+            }
+        }
+        if (nodesVisited < numCourses) {
+            return new int[]{};
+        } else {
+            return res;
+        }
+    }
+}
