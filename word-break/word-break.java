1ba79a3c149f913b7aa6baf8d5e9b12cf7513871
cheny-414 <94153287+cheny-414@users.noreply.github.com>
2023-07-10 16:26:00 -0400

    Time: 1 ms (99.88%) | Memory: 41.3 MB (84.43%) - LeetSync

--- word-break/word-break.java
+++ word-break/word-break.java
@@ -1,28 +1,36 @@
 class Solution {
-    public boolean wordBreak(String s, List<String> wordDict) {
-        Set<String> words = new HashSet<>(wordDict);
-        Queue<Integer> queue = new LinkedList<>();
-        boolean[] seen = new boolean[s.length() + 1];
-        queue.add(0);
+    private String s;
+    private List<String> wordDict;
+    private int[] memo;
+    
+    private boolean dp(int i) {
+        if (i < 0) return true;
         
-        while (!queue.isEmpty()) {
-            int start = queue.remove();
-            if (start == s.length()) {
-                return true;
+        if (memo[i] != -1) {
+            return memo[i] == 1;
+        }
+        
+        for (String word: wordDict) {
+            // Handle out of bounds case
+            if (i - word.length() + 1 < 0) {
+                continue;
             }
             
-            for (int end = start + 1; end <= s.length(); end++) {
-                if (seen[end]) {
-                    continue;
-                }
-                
-                if (words.contains(s.substring(start, end))) {
-                    queue.add(end);
-                    seen[end] = true;
-                }
+            if (s.substring(i - word.length() + 1, i + 1).equals(word) && dp(i - word.length())) {
+                memo[i] = 1;
+                return true;
             }
         }
         
+        memo[i] = 0;
         return false;
+    }
+    
+    public boolean wordBreak(String s, List<String> wordDict) {
+        this.s = s;
+        this.wordDict = wordDict;
+        this.memo = new int[s.length()];
+        Arrays.fill(this.memo, -1);
+        return dp(s.length() - 1);
     }
 }
