387606ea931a5c941d77dac52b70c95d302ef7a5
cheny-414 <94153287+cheny-414@users.noreply.github.com>
2023-07-12 23:16:33 -0400

    Time: 1 ms (96.86%) | Memory: 44 MB (40.29%) - LeetSync

--- dungeon-game/dungeon-game.java
+++ dungeon-game/dungeon-game.java
@@ -5,14 +5,17 @@ class Solution {
         int[][] dp = new int[m][n];
         dp[m - 1][n - 1] = dungeon[m - 1][n - 1] < 0 ? -dungeon[m - 1][n - 1] + 1 : 1;
 
+        //fill last col dp
         for (int i = m - 2; i >= 0; i--) {
             dp[i][n - 1] = Math.max(1, dp[i+1][n - 1] - dungeon[i][n - 1]);
         }
 
+        //fill last row dp
         for (int i = n - 2; i >= 0; i--) {
             dp[m - 1][i] = Math.max(1, dp[m - 1][i + 1] - dungeon[m - 1][i]);
         }
 
+        //fill the rest of dp
         for (int i = m - 2; i >= 0; i--) {
             for (int j = n - 2; j >= 0; j--) {
                 int lowerMin = Math.min(dp[i+1][j], dp[i][j+1]);
