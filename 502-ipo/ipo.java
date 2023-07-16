f573d367b59336db518d1bd5fd5e1522f27c2a33
cheny-414 <94153287+cheny-414@users.noreply.github.com>
2023-07-15 23:31:07 -0400

    Time: 117 ms (49.33%) | Memory: 56.5 MB (92.87%) - LeetSync

--- 502-ipo/ipo.java
+++ 502-ipo/ipo.java
@@ -4,14 +4,17 @@ class Solution {
         int[][] data = new int[n][2];
         int count = 0;
         int pointer = 0;
-        
-        PriorityQueue<int[]> q = new PriorityQueue<>((a, b) -> a[0] == b[0] ? a[1] - b[1] : b[0] - a[0]);
 
         for (int i = 0; i < n; i++) {
             data[i][0] = profits[i];
             data[i][1] = capital[i];
         }
+
+        //Capital ascending, then profits descending
         Arrays.sort(data, (a, b) -> a[1] == b[1] ? b[0] - a[0] : a[1] - b[1]);
+
+        //PriorityQueue: Profits descending
+        PriorityQueue<int[]> q = new PriorityQueue<>((a, b) -> b[0] - a[0]);
 
         while (count < k) {
             //enqueue affordable projects
