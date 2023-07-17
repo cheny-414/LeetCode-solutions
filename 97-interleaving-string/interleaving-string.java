4700eb43a7cd27e285a9550205449de3ce90066b
cheny-414 <94153287+cheny-414@users.noreply.github.com>
2023-07-17 01:27:34 -0400

    Time: 1 ms (95.08%) | Memory: 41.2 MB (25.84%) - LeetSync

--- 97-interleaving-string/interleaving-string.java
+++ 97-interleaving-string/interleaving-string.java
@@ -1,31 +1,32 @@
 class Solution {
     String s1, s2, s3;
+    int[][] dp;
     public boolean isInterleave(String s1, String s2, String s3) {
         if (s1.length() + s2.length() != s3.length()) return false;
         this.s1 = s1;
         this.s2 = s2;
         this.s3 = s3;
-        int[][] dp = new int[s1.length() + 1][s2.length() + 1];
+        this.dp = new int[s1.length() + 1][s2.length() + 1];
         for (int[] row : dp) {
             Arrays.fill(row, -1);
         }
-        boolean result = helper(0, 0, dp);
-        return result;
+        return helper(0, 0);
     }
 
-    private boolean helper(int i, int j, int[][] dp) {
+    private boolean helper(int i, int j) {
         if (i == s1.length() && j == s2.length()) return true;
 
         if (dp[i][j] != -1) return dp[i][j] == 1;
 
-        boolean choose_s1 = false, choose_s2 = false;
-        if (i < s1.length() && s1.charAt(i) == s3.charAt(i + j)) {
-            choose_s1 = helper(i + 1, j, dp);
+        if (i < s1.length() && j < s2.length() && s1.charAt(i) == s2.charAt(j) && s2.charAt(j) == s3.charAt(i + j)) {
+            dp[i][j] = (helper(i + 1, j) | helper(i, j + 1)) ? 1 : 0;
+        } else if (i < s1.length() && s1.charAt(i) == s3.charAt(i + j)) {
+            dp[i][j] = helper(i + 1, j) ? 1 : 0;
+        } else if (j < s2.length() && s2.charAt(j) == s3.charAt(i + j)) {
+            dp[i][j] = helper(i, j + 1) ? 1 : 0;
+        } else {
+            dp[i][j] = 0;
         }
-        if (j < s2.length() && s2.charAt(j) == s3.charAt(i + j)) {
-            choose_s2 = helper(i, j + 1, dp);
-        }
-        dp[i][j] = choose_s1 | choose_s2 ? 1 : 0;
         return dp[i][j] == 1;
     }
 }
