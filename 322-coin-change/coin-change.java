776a6b9facafbc8481ad1159609581e4f333f396
cheny-414 <94153287+cheny-414@users.noreply.github.com>
2023-07-16 21:20:37 -0400

    Time: 41 ms (21.30%) | Memory: 43.4 MB (64.36%) - LeetSync

---
+++ 322-coin-change/coin-change.java
@@ -0,0 +1,27 @@
+class Solution {
+    int[] dp;
+    public int coinChange(int[] coins, int amount) {
+        dp = new int[amount + 1];
+        Arrays.fill(dp, -2);
+        dp[0] = 0;
+        return helper(coins, dp, amount);
+    }
+
+    private int helper(int[] coins, int[] dp, int amount) {
+        if (amount == 0) return 0;
+        if (dp[amount] != -2) return dp[amount];
+        int minCoins = Integer.MAX_VALUE;
+        for (int coin : coins) {
+            if (coin <= amount) {
+                int remainingCoins = helper(coins, dp, amount - coin);
+                int coinsNeeded = remainingCoins == -1 ? -1 : remainingCoins + 1;
+                if (coinsNeeded != -1 && coinsNeeded < minCoins) {
+                    minCoins = coinsNeeded;
+                }
+            }
+        }
+
+        dp[amount] = minCoins == Integer.MAX_VALUE ? -1 : minCoins;
+        return dp[amount];
+    }
+}
