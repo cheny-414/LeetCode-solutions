fe1e1760a66f408a76f90d7a5d029a967c348f13
cheny-414 <94153287+cheny-414@users.noreply.github.com>
2023-07-22 20:47:26 -0400

    Time: 39 ms (85.67%) | Memory: 82.5 MB (73.72%) - LeetSync

---
+++ 2485-finding-the-number-of-visible-mountains/finding-the-number-of-visible-mountains.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int visibleMountains(int[][] peaks) {
+        int answer = 0;
+        for (int[] peak : peaks) {
+            int height = peak[1];
+            int pos = peak[0];
+            peak[0] = pos - height;
+            peak[1] = pos + height;
+        }
+        Arrays.sort(peaks, (a, b) -> a[0] == b[0] ? a[1] - b[1] : a[0] - b[0]);
+
+        int i = 0;
+        while(i < peaks.length){
+            answer++;
+            if(i + 1 == peaks.length){
+                return answer;
+            } 
+            int currEnd = peaks[i][1];
+            if(peaks[i][0] == peaks[i + 1][0]){
+                answer--;
+            }
+            while(i + 1 != peaks.length && currEnd >= peaks[i + 1][1]){
+                    i++;
+            }
+            i++;
+        }
+
+        return answer;
+    }
+}
