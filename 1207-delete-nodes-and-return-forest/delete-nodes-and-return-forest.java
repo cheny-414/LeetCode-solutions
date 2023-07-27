5d41d05f7574a9ac393bbcfe22f106df57f30309
cheny-414 <94153287+cheny-414@users.noreply.github.com>
2023-07-27 00:45:59 -0400

    Time: 1 ms (99.39%) | Memory: 44 MB (78.60%) - LeetSync

--- 1207-delete-nodes-and-return-forest/delete-nodes-and-return-forest.java
+++ 1207-delete-nodes-and-return-forest/delete-nodes-and-return-forest.java
@@ -14,42 +14,26 @@
  * }
  */
 class Solution {
-    Map<Integer, TreeNode> map = new HashMap<>();
-    Map<TreeNode, Pair<TreeNode, Boolean>> parents = new HashMap<>();
     public List<TreeNode> delNodes(TreeNode root, int[] to_delete) {
-        Set<TreeNode> set = new HashSet<>();
-        set.add(root);
-        dfs(root, null, false);
-        for (int i = 0; i < to_delete.length; i++) {
-            TreeNode del = map.get(to_delete[i]);
-            if (set.contains(del)) {
-                set.remove(del);
-            }
-            if (del.left != null) {
-                set.add(del.left);
-            }
-            if (del.right != null) {
-                set.add(del.right);
-            }
-            TreeNode parent = parents.get(del).getKey();
-            boolean left = parents.get(del).getValue();
-            if (parent != null) {
-                if (left) {
-                    parent.left = null;
-                } else {
-                    parent.right = null;
-                }
-            }
-        }
-        List<TreeNode> answer = new ArrayList<TreeNode>(set);
-        return answer;
+        Set<Integer> set = new HashSet<>();
+        for (int i : to_delete) set.add(i);
+        List<TreeNode> res = new ArrayList<>();
+        if (!set.contains(root.val)) res.add(root);
+        dfs(root, set, res);
+        return res;
     }
 
-    private void dfs(TreeNode root, TreeNode parent, boolean left) {
-        if (root == null) return;
-        map.put(root.val, root);
-        parents.put(root, new Pair<>(parent, left));
-        dfs(root.left, root, true);
-        dfs(root.right, root, false);
+    private TreeNode dfs(TreeNode node, Set<Integer> set, List<TreeNode> res) {
+        if (node == null) {
+            return null;
+        }
+        node.left = dfs(node.left, set, res);
+        node.right = dfs(node.right, set, res);
+        if (set.contains(node.val)) {
+            if (node.left != null) res.add(node.left);
+            if (node.right != null) res.add(node.right);
+            return null;
+        }
+        return node;
     }
 }
