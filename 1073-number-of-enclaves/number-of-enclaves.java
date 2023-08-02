b7a1c3fee7e7992a58b881345ab1405327e5a9db
cheny-414 <94153287+cheny-414@users.noreply.github.com>
2023-08-02 17:23:55 -0400

    Time: 6 ms (99.90%) | Memory: 54.8 MB (51.10%) - LeetSync

---
+++ 1073-number-of-enclaves/number-of-enclaves.java
@@ -0,0 +1,48 @@
+class Solution {
+    int[][] grid;
+    int[][] directions = new int[][] {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+    public int numEnclaves(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        this.grid = grid;
+        int answer = 0;
+
+        for (int i = 0; i < m; i++) {
+            if (grid[i][0] == 1) {
+                dfs(i, 0);
+            }
+            if (grid[i][n - 1] == 1){
+                dfs(i, n - 1);
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (grid[0][i] == 1) {
+                dfs(0, i);
+            }
+            if (grid[m - 1][i] == 1){
+                dfs(m - 1, i);
+            }
+        }
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1) {
+                    answer++;
+                }
+            }
+        }
+        return answer;
+    }
+
+    private void dfs(int row, int col) {
+        grid[row][col] = 2;
+        for (int[] dir : directions) {
+            int nextrow = row + dir[0];
+            int nextcol = col + dir[1];
+            if (nextrow >= 0 && nextrow < grid.length && nextcol >= 0 && nextcol < grid[0].length && grid[nextrow][nextcol] == 1) {
+                dfs(nextrow, nextcol);
+            }
+        }
+    }
+}
