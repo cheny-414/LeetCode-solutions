4fe3b473feaa4040e29ac92ee9b0d5b56b05e90a
cheny-414 <94153287+cheny-414@users.noreply.github.com>
2023-08-03 15:47:57 -0400

    Time: 13 ms (51.63%) | Memory: 45.1 MB (78.56%) - LeetSync

---
+++ 1101-parallel-courses/parallel-courses.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int minimumSemesters(int n, int[][] relations) {
+        Deque<Integer> q = new ArrayDeque<>();
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        int[] indegrees = new int[n + 1];
+        int coursesTaken = 0;
+        int semesters = 0;
+
+        for (int[] relation : relations) {
+            int prev = relation[0];
+            int next = relation[1];
+            if (!map.containsKey(prev)) {
+                map.put(prev, new ArrayList<>());
+            }
+            map.get(prev).add(next);
+            indegrees[next]++;
+        }
+
+        for (int i  = 1; i < n; i++) {
+            if (indegrees[i] == 0) {
+                q.add(i);
+            }
+        }
+
+        while (!q.isEmpty()) {
+            int size = q.size();
+            for (int i = 0; i < size; i++) {
+                int course = q.pop();
+                coursesTaken++;
+                if (!map.containsKey(course)) continue;
+                for (int nextCourse : map.get(course)) {
+                    indegrees[nextCourse]--;
+                    if (indegrees[nextCourse] == 0) {
+                        q.add(nextCourse);
+                    }
+                }
+            }
+            semesters++;
+        }
+        return coursesTaken == n ? semesters : -1;
+    }
+}
