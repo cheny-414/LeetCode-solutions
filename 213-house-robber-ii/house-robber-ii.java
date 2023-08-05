6ebe070087d1a58b893dacfdefa81e90af112666
cheny-414 <94153287+cheny-414@users.noreply.github.com>
2023-08-05 18:25:24 -0400

    Time: 0 ms (100.00%) | Memory: 39.6 MB (91.59%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.java
+++ 213-house-robber-ii/house-robber-ii.java
@@ -8,16 +8,16 @@ class Solution {
         int[] dp2 = new int[nums.length];
         Arrays.fill(dp1, -1);
         Arrays.fill(dp2, -1);
-        return Math.max(solve(dp1, 0, 0, nums.length - 2), solve(dp2, 1, 1, nums.length - 1));
+        return Math.max(solve(dp1, 0, nums.length - 2), solve(dp2, 1, nums.length - 1));
     }
 
-    private int solve(int[] dp, int start, int left, int right) {
+    private int solve(int[] dp, int start, int right) {
         if (start > right) {
             return 0;
         }
         if (dp[start] != -1) return dp[start];
 
-        dp[start] = Math.max(solve(dp, start + 1, left, right), solve(dp, start + 2, left, right) + nums[start]);
+        dp[start] = Math.max(solve(dp, start + 1, right), solve(dp, start + 2, right) + nums[start]);
         return dp[start];
     }
 }
