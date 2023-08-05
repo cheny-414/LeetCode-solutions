869bfacb3d23ce02129d572fe6de2b7ccb24861e
cheny-414 <94153287+cheny-414@users.noreply.github.com>
2023-08-05 03:13:29 -0400

    Time: 22 ms (48.46%) | Memory: 43.8 MB (34.48%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -1,27 +1,27 @@
 class Solution {
+    int[] coins;
     int[] dp;
     public int coinChange(int[] coins, int amount) {
+        this.coins = coins;
         dp = new int[amount + 1];
         Arrays.fill(dp, -2);
-        dp[0] = 0;
-        return helper(coins, amount);
+        int result = solve(amount);
+        return result == Integer.MAX_VALUE ? -1 : result;
     }
 
-    private int helper(int[] coins, int amount) {
+    private int solve(int amount) {
         if (amount == 0) return 0;
         if (dp[amount] != -2) return dp[amount];
-        int minCoins = Integer.MAX_VALUE;
+        int minNeeded = Integer.MAX_VALUE;
         for (int coin : coins) {
             if (coin <= amount) {
-                int remainingCoins = helper(coins, amount - coin);
-                int coinsNeeded = remainingCoins == -1 ? -1 : remainingCoins + 1;
-                if (coinsNeeded != -1 && coinsNeeded < minCoins) {
-                    minCoins = coinsNeeded;
+                int remainingNeeded = solve(amount - coin);
+                if (remainingNeeded != Integer.MAX_VALUE) {
+                    minNeeded = Math.min(1 + remainingNeeded, minNeeded);
                 }
             }
         }
-
-        dp[amount] = minCoins == Integer.MAX_VALUE ? -1 : minCoins;
+        dp[amount] = minNeeded;
         return dp[amount];
     }
 }
