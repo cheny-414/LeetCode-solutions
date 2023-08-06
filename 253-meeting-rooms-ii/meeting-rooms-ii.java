7da135c4c232a18bfd5d82533902988d2f593e5c
cheny-414 <94153287+cheny-414@users.noreply.github.com>
2023-08-06 14:53:17 -0400

    Time: 9 ms (35.03%) | Memory: 44.4 MB (76.32%) - LeetSync

--- 253-meeting-rooms-ii/meeting-rooms-ii.java
+++ 253-meeting-rooms-ii/meeting-rooms-ii.java
@@ -2,15 +2,17 @@ class Solution {
     public int minMeetingRooms(int[][] intervals) {
         Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
         PriorityQueue<Integer> pq = new PriorityQueue<>();
+        int maxSize = 0;
 
         for (int[] interval : intervals) {
             int startTime = interval[0];
             int endTime = interval[1];
-            if (!pq.isEmpty() && pq.peek() <= startTime) {
+            while (!pq.isEmpty() && pq.peek() <= startTime) {
                 pq.poll();
             }
             pq.offer(endTime);
+            maxSize = Math.max(pq.size(), maxSize);
         }
-        return pq.size();
+        return maxSize;
     }
 }
