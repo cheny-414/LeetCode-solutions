4137ade0ad4b49c79175d16ca635e578dbc5370e
cheny-414 <94153287+cheny-414@users.noreply.github.com>
2023-08-06 23:48:55 -0400

    Time: 1 ms (99.88%) | Memory: 59.5 MB (38.52%) - LeetSync

---
+++ 41-first-missing-positive/first-missing-positive.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        //convert all positive numbers to 0, since negative numbers don't matter
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] < 0) {
+                nums[i] = 0;
+            }
+        }
+        for (int i = 0; i < nums.length; i++) {
+            int val = Math.abs(nums[i]);
+            if (val >= 1 && val <= nums.length) {
+                if (nums[val - 1] > 0) {
+                    nums[val - 1] *= -1;
+                }
+                else if (nums[val - 1] == 0) {
+                    nums[val - 1] = Integer.MIN_VALUE;
+                }
+            }
+        }
+        for (int i = 1; i < nums.length + 1; i++) {
+            if (nums[i - 1] >= 0) return i;
+        }
+        return nums.length + 1;
+    }
+}
