f8ec6ac85c2742f1d69dc3bbcce8c30359e70b36
cheny-414 <94153287+cheny-414@users.noreply.github.com>
2023-08-07 02:02:13 -0400

    Time: 1 ms (100.00%) | Memory: 41.3 MB (37.59%) - LeetSync

--- 10-regular-expression-matching/regular-expression-matching.java
+++ 10-regular-expression-matching/regular-expression-matching.java
@@ -3,19 +3,23 @@ class Solution {
     int n;
     String s;
     String p;
-    int[][] dp;
+    int[][] dp; //1 is true, 2 is false
     public boolean isMatch(String s, String p) {
         this.s = s;
         this.p = p;
         this.m = s.length();
         this.n = p.length();
-        this.dp = new int[m][n];
+        this.dp = new int[m + 1][n + 1];
         return solve(0, 0);
     }
 
     private boolean solve(int i, int j) {
         if (i >= m && j >= n) return true;
         if (j >= n) return false;
+
+        if (dp[i][j] != 0) {
+            return dp[i][j] == 1;
+        }
 
         boolean canSolve = false;
         boolean hasStar = j < n - 1 && p.charAt(j + 1) == '*';
@@ -25,12 +29,13 @@ class Solution {
             canSolve = canSolve || solve(i + 1, j); //use star
         }
         if (hasStar) {
-            canSolve = canSolve || solve(i, j + 2);
+            canSolve = canSolve || solve(i, j + 2); //not use star
         }
         if (isMatch) {
-            canSolve = canSolve || solve(i + 1, j + 1);
+            canSolve = canSolve || solve(i + 1, j + 1); //move to next character
         }
-
+        
+        dp[i][j] = canSolve ? 1 : 2;
         return canSolve;
     }
 }
