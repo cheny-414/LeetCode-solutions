5f25216a0d97cb564c21602d6b06462f5ab6c5c9
cheny-414 <94153287+cheny-414@users.noreply.github.com>
2023-08-07 21:21:30 -0400

    Time: 20 ms (83.34%) | Memory: 40.9 MB (98.44%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,34 +1,32 @@
 class Solution {
     public String longestPalindrome(String s) {
-        int n = s.length();
-        boolean[][] dp = new boolean[n][n];
-        int[] ans = new int[]{0, 0};
-        
-        for (int i = 0; i < n; i++) {
-            dp[i][i] = true;
-        }
-        
-        for (int i = 0; i < n - 1; i++) {
-            if (s.charAt(i) == s.charAt(i + 1)) {
-                dp[i][i + 1] = true;
-                ans[0] = i;
-                ans[1] = i + 1;
-            }
-        }
-        
-        for (int diff = 2; diff < n; diff++) {
-            for (int i = 0; i < n - diff; i++) {
-                int j = i + diff;
-                if (s.charAt(i) == s.charAt(j) && dp[i + 1][j - 1]) {
-                    dp[i][j] = true;
-                    ans[0] = i;
-                    ans[1] = j;
+        int maxLength = 0;
+        int globalLeft = -1;
+        int globalRight = -1;
+
+        for (int i = 0; i < s.length(); i++) {
+            int left = i, right = i;
+            while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+                if (right - left + 1 > maxLength) {
+                    maxLength = right - left + 1;
+                    globalLeft = left;
+                    globalRight = right;
                 }
+                left--;
+                right++;
+            }
+
+            left = i; right = i + 1;
+            while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+                if (right - left + 1 > maxLength) {
+                    maxLength = right - left + 1;
+                    globalLeft = left;
+                    globalRight = right;
+                }
+                left--;
+                right++;
             }
         }
-        
-        int i = ans[0];
-        int j = ans[1];
-        return s.substring(i, j + 1);
+        return s.substring(globalLeft, globalRight + 1);
     }
 }
