bfece1ce41a1442d6d69bef99e0c101c716c6294
cheny-414 <94153287+cheny-414@users.noreply.github.com>
2023-08-07 21:43:37 -0400

    Time: 26 ms (96.94%) | Memory: 54.7 MB (36.02%) - LeetSync

--- 516-longest-palindromic-subsequence/longest-palindromic-subsequence.java
+++ 516-longest-palindromic-subsequence/longest-palindromic-subsequence.java
@@ -14,16 +14,13 @@ class Solution {
 
     private int solve(int left, int right) {
         if (left == right) return 1;
-        //if (left + 1 == right && s.charAt(left) == s.charAt(right)) return 2;
         if (left > right) return 0;
         if (dp[left][right] != -1) return dp[left][right];
 
         int answer = 0;
         if (s.charAt(left) == s.charAt(right)) {
             int inside = solve(left + 1, right - 1);
-            if (inside >= 0) {
-                answer = inside + 2;
-            }
+            answer = inside + 2;
         } else {
             answer = Math.max(solve(left + 1, right), solve(left, right - 1));
         }
