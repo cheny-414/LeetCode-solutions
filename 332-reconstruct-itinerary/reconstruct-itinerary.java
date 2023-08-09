b0ebf85d5a01861dce77ef3ff449844735184efa
cheny-414 <94153287+cheny-414@users.noreply.github.com>
2023-08-09 18:02:14 -0400

    Time: 6 ms (72.51%) | Memory: 43.9 MB (99.27%) - LeetSync

--- 332-reconstruct-itinerary/reconstruct-itinerary.java
+++ 332-reconstruct-itinerary/reconstruct-itinerary.java
@@ -10,13 +10,12 @@ class Solution {
           targets.get(ticket.get(0)).add(ticket.get(1));
         }
         visit("JFK", targets);
-        Collections.reverse(answer);
         return answer;
     }
     public void visit(String airport, Map<String, PriorityQueue<String>> targets) {
         while (targets.containsKey(airport) && !targets.get(airport).isEmpty()) {
             visit(targets.get(airport).poll(), targets);
         }
-        answer.add(airport);
+        answer.add(0, airport);
     }
 }
