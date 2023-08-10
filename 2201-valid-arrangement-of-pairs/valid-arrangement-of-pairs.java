7a2abe8f8f3a615bd51a80e623c48ab0d10fff9d
cheny-414 <94153287+cheny-414@users.noreply.github.com>
2023-08-09 20:38:13 -0400

    Time: 152 ms (91.11%) | Memory: 132.6 MB (13.33%) - LeetSync

--- 2201-valid-arrangement-of-pairs/valid-arrangement-of-pairs.java
+++ 2201-valid-arrangement-of-pairs/valid-arrangement-of-pairs.java
@@ -1,58 +1,41 @@
 class Solution {
-    // map to track all outcoming edges for given vertex
     HashMap<Integer, Stack<Integer>> map = new HashMap<>();
-
+    List<Integer> path;
     public int[][] validArrangement(int[][] pairs) {
-        
-        // map to calculate outdegree - indegree
-        HashMap<Integer, Integer> degrees = new HashMap<>();
+        HashMap<Integer, Integer> degree = new HashMap<>();
 
-        for (int[] p : pairs) {
-            if ( ! map.containsKey(p[0])) {
-                map.put(p[0], new Stack<>());
-            }
-            map.get(p[0]).push(p[1]);
-
-            degrees.put(p[0], degrees.getOrDefault(p[0], 0)-1);
-            degrees.put(p[1], degrees.getOrDefault(p[1], 0)+1);
+        for (int[] pair : pairs) {
+            map.putIfAbsent(pair[0], new Stack<>());
+            map.get(pair[0]).push(pair[1]);
+            degree.put(pair[0], degree.getOrDefault(pair[0], 0) - 1);
+            degree.put(pair[1], degree.getOrDefault(pair[1], 0) + 1);
         }
 
-        /*
-            Based on task description our graph at least has Eulerian path
-            or possible even circle.
-            If it has circle, we can start from any node (let's take the first vertex)
-            If the graph has Eulerian path only, in this case there will be exactly
-            one vertex with degree -1, this will be our start node.
-        */
         int start = pairs[0][0];
-        for (int[] p : pairs) {
-            if (degrees.get(p[0]) == -1) {
-                start = p[0];
+        for (int[] pair : pairs) {
+            if (degree.get(pair[0]) == -1) {
+                start = pair[0];
                 break;
             }
         }
 
-        // build the path in reverse order
-        List<Integer> path = new ArrayList<>();        
-        calculate(start, path);
-        // reverse it
+        path = new ArrayList<>();
+        dfs(start);
         Collections.reverse(path);
 
-        int[][] res = new int[pairs.length][2];
+        int[][] result = new int[pairs.length][2];
 
-        // build the result
         for (int i = 1; i < path.size(); i++) {
-            res[i-1][0] = path.get(i-1);
-            res[i-1][1] = path.get(i);
+            result[i - 1][0] = path.get(i - 1);
+            result[i - 1][1] = path.get(i);
         }
-
-        return res;
+        return result;
     }
 
-    public void calculate(int v, List<Integer> path) {
-        while (map.containsKey(v) && map.get(v).size() > 0) {
-            calculate(map.get(v).pop(), path);
+    private void dfs(int node) {
+        while (map.containsKey(node) && !map.get(node).isEmpty()) {
+            dfs(map.get(node).pop());
         }
-        path.add(v);
+        path.add(node);
     }
 }
