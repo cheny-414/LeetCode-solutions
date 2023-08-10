bb18f0640d84e9383f7752675f1d6649dbea9a2f
cheny-414 <94153287+cheny-414@users.noreply.github.com>
2023-08-09 20:34:23 -0400

    Time: 164 ms (84.44%) | Memory: 134.2 MB (11.11%) - LeetSync

--- 2201-valid-arrangement-of-pairs/valid-arrangement-of-pairs.java
+++ 2201-valid-arrangement-of-pairs/valid-arrangement-of-pairs.java
@@ -1,46 +1,58 @@
 class Solution {
-    Map<Integer, List<Integer>> adj = new HashMap<>();
-    List<List<Integer>> ans = new ArrayList<>();
-    
-    public int[][] validArrangement(int[][] pairs) {
-        Map<Integer, Integer> indegree = new HashMap<>();
-        for(int[] pair : pairs) {
-            adj.putIfAbsent(pair[0], new ArrayList<>());
-            adj.get(pair[0]).add(pair[1]);
-            indegree.put(pair[0], indegree.getOrDefault(pair[0], 0) + 1);
-            indegree.put(pair[1], indegree.getOrDefault(pair[1], 0) - 1);
+    // map to track all outcoming edges for given vertex
+    HashMap<Integer, Stack<Integer>> map = new HashMap<>();
 
-        }
+    public int[][] validArrangement(int[][] pairs) {
         
-        int start = -1;
-        for (Integer key: indegree.keySet()) {
-            if (indegree.get(key) > 0)  {
-                start = key;
+        // map to calculate outdegree - indegree
+        HashMap<Integer, Integer> degrees = new HashMap<>();
+
+        for (int[] p : pairs) {
+            if ( ! map.containsKey(p[0])) {
+                map.put(p[0], new Stack<>());
+            }
+            map.get(p[0]).push(p[1]);
+
+            degrees.put(p[0], degrees.getOrDefault(p[0], 0)-1);
+            degrees.put(p[1], degrees.getOrDefault(p[1], 0)+1);
+        }
+
+        /*
+            Based on task description our graph at least has Eulerian path
+            or possible even circle.
+            If it has circle, we can start from any node (let's take the first vertex)
+            If the graph has Eulerian path only, in this case there will be exactly
+            one vertex with degree -1, this will be our start node.
+        */
+        int start = pairs[0][0];
+        for (int[] p : pairs) {
+            if (degrees.get(p[0]) == -1) {
+                start = p[0];
+                break;
             }
         }
-        
-        if(start == -1) {
-            start = pairs[0][0];
-        }
-        
-        dfs(start);
-        
-        int[][] result = new int[pairs.length][pairs[0].length];
 
-        for (int i = 0; i < ans.size(); i++) {
-            result[i][0] = ans.get(ans.size() - i - 1).get(0);
-            result[i][1] = ans.get(ans.size() - i - 1).get(1);
+        // build the path in reverse order
+        List<Integer> path = new ArrayList<>();        
+        calculate(start, path);
+        // reverse it
+        Collections.reverse(path);
+
+        int[][] res = new int[pairs.length][2];
+
+        // build the result
+        for (int i = 1; i < path.size(); i++) {
+            res[i-1][0] = path.get(i-1);
+            res[i-1][1] = path.get(i);
         }
-        return result;
+
+        return res;
     }
-    
-    private void dfs(int node) {
-        while(adj.get(node) != null && adj.get(node).size() > 0) {
-            List<Integer> nodes = adj.get(node);
-            int next = nodes.get(nodes.size() - 1);
-            adj.get(node).remove(nodes.size() - 1);
-            dfs(next);
-            ans.add(Arrays.asList(node, next));
+
+    public void calculate(int v, List<Integer> path) {
+        while (map.containsKey(v) && map.get(v).size() > 0) {
+            calculate(map.get(v).pop(), path);
         }
+        path.add(v);
     }
 }
