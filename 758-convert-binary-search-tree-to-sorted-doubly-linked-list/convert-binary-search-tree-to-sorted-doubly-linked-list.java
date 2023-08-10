ed7e87d719b097a86f95c49a3191046d8512f763
cheny-414 <94153287+cheny-414@users.noreply.github.com>
2023-08-10 17:55:38 -0400

    Time: 1 ms (19.40%) | Memory: 41.2 MB (18.31%) - LeetSync

---
+++ 758-convert-binary-search-tree-to-sorted-doubly-linked-list/convert-binary-search-tree-to-sorted-doubly-linked-list.java
@@ -0,0 +1,40 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val,Node _left,Node _right) {
+        val = _val;
+        left = _left;
+        right = _right;
+    }
+};
+*/
+
+class Solution {
+    public Node treeToDoublyList(Node root) {
+        List<Node> list = new ArrayList<>();
+        if (root == null) return null;
+        dfs(root, list);
+        for (int i = 0; i < list.size(); i++) {
+            list.get(i).left = i == 0 ? list.get(list.size() - 1) : list.get(i - 1);
+            list.get(i).right = i == list.size() - 1 ? list.get(0) : list.get(i + 1);
+        }
+        return list.get(0);
+    }
+
+    private void dfs(Node root, List<Node> list) {
+        if (root == null) return;
+        dfs(root.left, list);
+        list.add(root);
+        dfs(root.right, list);
+    }
+}
