9495a2b30912ae48917c3caac1718b7fab2fdfab
cheny-414 <94153287+cheny-414@users.noreply.github.com>
2023-08-10 22:52:52 -0400

    Time: 1 ms (6.82%) | Memory: 40.6 MB (87.22%) - LeetSync

--- 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.java
+++ 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.java
@@ -15,19 +15,38 @@
  */
 class Solution {
     public List<Integer> preorderTraversal(TreeNode root) {
+        BSTIterator it = new BSTIterator(root);
         List<Integer> result = new ArrayList<>();
-        Stack<TreeNode> stack = new Stack<>();
-        TreeNode curr = root;
-        while (!stack.isEmpty() || curr != null) {
-            if (curr != null) {
-                stack.push(curr);
-                result.add(curr.val);
-                curr = curr.left;
-            } else {
-                TreeNode node = stack.pop();
-                curr = node.right;
-            }
+        while (it.hasNext()) {
+            result.add(it.next());
         }
         return result;
+    }
+}
+
+class BSTIterator {
+    private Stack<TreeNode> stack;
+    public BSTIterator(TreeNode root) {
+        stack = new Stack<>();
+        TreeNode curr = root;
+        if (curr != null) {
+            stack.push(curr);
+        }
+    }
+    
+    public int next() {
+        TreeNode node = stack.pop();
+        if (node.right != null) {
+            stack.push(node.right);
+        } 
+        if (node.left != null) {
+            stack.push(node.left);
+        }
+
+        return node.val;
+    }
+    
+    public boolean hasNext() {
+        return !stack.isEmpty();
     }
 }
