0964255f206f85cdaabb21d0a27db968bf5e6c5e
cheny-414 <94153287+cheny-414@users.noreply.github.com>
2023-08-15 02:30:28 -0400

    Time: 17 ms (87.21%) | Memory: 43.3 MB (64.05%) - LeetSync

--- 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.java
+++ 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.java
@@ -2,33 +2,29 @@ class Solution {
     public long subArrayRanges(int[] nums) {
         int n = nums.length;
         long answer = 0;
-        long[][][] dp = new long[n][n+1][2]; //startIndex, length, (range, max)
+        Stack<Integer> stack = new Stack<>();    
 
-        for (int i = 0; i < n; i++) {
-            dp[i][1][1] = nums[i];
-        }
-        for (int length = 2; length <= n; length++) {
-            for (int start = 0; start + length - 1 < n; start++) {
-                int end = start + length - 1;
-                long lastMax = dp[start][length - 1][1];
-                long lastMin = dp[start][length - 1][1] - dp[start][length - 1][0];
-                long value = nums[end];
-                if (value > lastMax) {
-                    dp[start][length][0] = dp[start][length - 1][0] + (value - lastMax);
-                    dp[start][length][1] = value;
-                } else if (value < lastMin) {
-                    dp[start][length][1] = lastMax;
-                    dp[start][length][0] = lastMax - value;
-                } else {
-                    dp[start][length][0] = dp[start][length - 1][0];
-                    dp[start][length][1] = lastMax;
-                }
+        // Find the sum of all the minimum.
+        for (int right = 0; right <= n; ++right) {
+            while (!stack.isEmpty() && (right == n || nums[stack.peek()] >= nums[right])) {
+                int mid = stack.peek();
+                stack.pop();
+                int left = stack.isEmpty() ? -1 : stack.peek();
+                answer -= (long)nums[mid] * (right - mid) * (mid - left);   
             }
+            stack.add(right);
         }
-        for (int i = 0; i < n; i++) {
-            for (int j = 1; j <= n; j++) {
-                answer += dp[i][j][0];
+        
+        // Find the sum of all the maximum.
+        stack.clear();
+        for (int right = 0; right <= n; ++right) {
+            while (!stack.isEmpty() && (right == n || nums[stack.peek()] <= nums[right])) {
+                int mid = stack.peek();
+                stack.pop();
+                int left = stack.isEmpty() ? -1 : stack.peek();
+                answer += (long)nums[mid] * (right - mid) * (mid - left);   
             }
+            stack.add(right);
         }
         return answer;
     }
