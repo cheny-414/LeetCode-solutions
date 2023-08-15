5cb98527a005759d1eef179c50d92c4a84d18606
cheny-414 <94153287+cheny-414@users.noreply.github.com>
2023-08-15 15:27:15 -0400

    Time: 1 ms (99.79%) | Memory: 46.6 MB (70.30%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.java
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.java
@@ -11,26 +11,16 @@
  */
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        ListNode a = headA;
-        ListNode b = headB;
-        boolean aCycled = false;
-        boolean bCycled = false;
-        while (a != b) {
-            if (a == null) {
-                if (aCycled) return null;
-                aCycled = true;
-                a = headB;
-            } else {
-                a = a.next;
-            }
-            if (b == null) {
-                if (bCycled) return null;
-                bCycled = true;
-                b = headA;
-            } else {
-                b = b.next;
-            }
+        ListNode pA = headA;
+        ListNode pB = headB;
+        while (pA != pB) {
+            pA = pA == null ? headB : pA.next;
+            pB = pB == null ? headA : pB.next;
         }
-        return a;
+        return pA;
+        // Note: In the case lists do not intersect, the pointers for A and B
+        // will still line up in the 2nd iteration, just that here won't be
+        // a common node down the list and both will reach their respective ends
+        // at the same time. So pA will be NULL in that case.
     }
 }
