d1ac99c626bf44b304dbafa9c1fe86672bf1111e
cheny-414 <94153287+cheny-414@users.noreply.github.com>
2023-08-19 19:40:42 -0400

    Time: 13 ms (92.05%) | Memory: 43.8 MB (85.39%) - LeetSync

--- 227-basic-calculator-ii/basic-calculator-ii.java
+++ 227-basic-calculator-ii/basic-calculator-ii.java
@@ -1,41 +1,28 @@
 class Solution {
     public int calculate(String s) {
-        Stack<Integer> nums = new Stack<>();
-        Stack<Character> ops = new Stack<>();
-        Set<Character> op = new HashSet<>(Arrays.asList('+', '-', '*', '/'));
-        int curr = 0;
-        int result;
-        for (char c : s.toCharArray()) {
-            if (Character.isDigit(c)) {
-                curr = curr * 10 + (c - '0');
-            } else if (op.contains(c)) {
-                nums.push(curr);
-                curr = 0;
-                while (!ops.isEmpty() && compare(ops.peek(), c)) {
-                    int val = evaluate(ops.pop(), nums.pop(), nums.pop());
-                    nums.push(val);
+        if (s == null || s.isEmpty()) return 0;
+        int length = s.length();
+        int currentNumber = 0, lastNumber = 0, result = 0;
+        char operation = '+';
+        for (int i = 0; i < length; i++) {
+            char currentChar = s.charAt(i);
+            if (Character.isDigit(currentChar)) {
+                currentNumber = (currentNumber * 10) + (currentChar - '0');
+            }
+            if (!Character.isDigit(currentChar) && !Character.isWhitespace(currentChar) || i == length - 1) {
+                if (operation == '+' || operation == '-') {
+                    result += lastNumber;
+                    lastNumber = (operation == '+') ? currentNumber : -currentNumber;
+                } else if (operation == '*') {
+                    lastNumber = lastNumber * currentNumber;
+                } else if (operation == '/') {
+                    lastNumber = lastNumber / currentNumber;
                 }
-                ops.push(c);
+                operation = currentChar;
+                currentNumber = 0;
             }
         }
-        nums.push(curr);
-        while (!ops.isEmpty()) {
-            int val = evaluate(ops.pop(), nums.pop(), nums.pop());
-            nums.push(val);
-        }
-        return nums.peek();
-    }
-
-    private boolean compare(Character a, Character b) {
-        int aVal = (a == '+' || a == '-') ? 1 : 2;
-        int bVal = (b == '+' || b == '-') ? 1 : 2;
-        return aVal >= bVal;
-    }
-
-    private int evaluate(Character op, int b, int a) {
-        if (op == '+') return a + b;
-        else if (op == '-') return a - b;
-        else if (op == '*') return a * b;
-        else return a / b;
+        result += lastNumber;
+        return result;
     }
 }
