f78940dfd4f8e1ced3d6c69a21a0f99a44917294
cheny-414 <94153287+cheny-414@users.noreply.github.com>
2023-08-20 15:08:34 -0400

    Time: 3 ms (58.89%) | Memory: 43.8 MB (90.23%) - LeetSync

---
+++ 47-permutations-ii/permutations-ii.java
@@ -0,0 +1,29 @@
+class Solution {
+    List<List<Integer>> res = new ArrayList<>();
+    boolean[] used;
+    int[] nums;
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        List<Integer> empty = new ArrayList<>();
+        used = new boolean[nums.length];
+        Arrays.sort(nums);
+        this.nums = nums;
+        dfs(empty);
+        return res;
+    }
+
+    private void dfs(List<Integer> curr) {
+        if (curr.size() == nums.length) {
+            res.add(new ArrayList<>(curr));
+            return;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (used[i]) continue;
+            if (i > 0 && nums[i] == nums[i - 1] && used[i - 1]) continue;
+            used[i] = true;
+            curr.add(nums[i]);
+            dfs(curr);
+            used[i] = false;
+            curr.remove(curr.size() - 1);
+        }
+    }
+}
