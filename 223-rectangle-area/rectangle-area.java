e5c0da5867e1147d57c5e4cb93b0c7cf746b7ce0
cheny-414 <94153287+cheny-414@users.noreply.github.com>
2023-08-20 02:48:26 -0400

    Time: 2 ms (73.33%) | Memory: 40.9 MB (96.41%) - LeetSync

--- 223-rectangle-area/rectangle-area.java
+++ 223-rectangle-area/rectangle-area.java
@@ -1,26 +1,5 @@
 class Solution {
     public int computeArea(int ax1, int ay1, int ax2, int ay2, int bx1, int by1, int bx2, int by2) {
-        if (ax1 > ax2) {
-            int temp = ax1;
-            ax1 = ax2;
-            ax2 = temp;
-        }
-        if (ay1 > ay2) {
-            int temp = ay1;
-            ay1 = ay2;
-            ay2 = temp;
-        }
-        if (bx1 > bx2) {
-            int temp = bx1;
-            bx1 = bx2;
-            bx2 = temp;
-        }
-        if (by1 > by2) {
-            int temp = by1;
-            by1 = by2;
-            by2 = temp;
-        }
-
         int area_a = (ax2 - ax1) * (ay2 - ay1);
         int area_b = (bx2 - bx1) * (by2 - by1);
 
