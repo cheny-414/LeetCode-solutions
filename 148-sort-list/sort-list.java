7ca22dfd53382a6155c4b1fbce98b6500994a330
cheny-414 <94153287+cheny-414@users.noreply.github.com>
2023-08-20 18:48:39 -0400

    Time: 9 ms (92.93%) | Memory: 56 MB (44.97%) - LeetSync

--- 148-sort-list/sort-list.java
+++ 148-sort-list/sort-list.java
@@ -9,27 +9,53 @@
  * }
  */
 class Solution {
+
+    //merge sort
     public ListNode sortList(ListNode head) {
-        if (head == null) return null;
-        ListNode headP = new ListNode();
-        headP.next = head;
-        ListNode curr = head;
-        ListNode p = headP;
-        
-        while (curr.next != null) {
-            if (curr.next.val > curr.val) {
-                curr = curr.next;
-            } else {
-                while (p.next.val < curr.next.val) {
-                    p = p.next;
-                }
-                ListNode temp = curr.next.next;
-                curr.next.next = p.next;
-                p.next = curr.next;
-                curr.next = temp;
-                p = headP;
-            }
+        //base case
+        if (head == null || head.next == null) return head;
+
+        //split list in two
+        ListNode mid = getMid(head);
+        ListNode temp = mid.next;
+        mid.next = null;
+
+        //sort each half
+        ListNode left = sortList(head);
+        ListNode right = sortList(temp);
+
+        //merge 2 halves
+        return merge(left, right);
+    }
+
+    private ListNode getMid(ListNode head) {
+        ListNode slow = head, fast = head.next;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
         }
-        return headP.next;
+        return slow;
+    }
+
+    private ListNode merge(ListNode left, ListNode right) {
+        ListNode prehead = new ListNode();
+        ListNode p = prehead;
+        while (left != null && right != null) {
+            if (left.val < right.val) {
+                p.next = left;
+                left = left.next;
+            } else {
+                p.next = right;
+                right = right.next;
+            }
+            p = p.next;
+        }
+        if (left != null) {
+            p.next = left;
+        }
+        if (right != null) {
+            p.next = right;
+        }
+        return prehead.next;
     }
 }
