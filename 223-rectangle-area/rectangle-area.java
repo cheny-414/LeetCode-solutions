749bf8f23db0f17aa9a1085e77ac6555352da75d
cheny-414 <94153287+cheny-414@users.noreply.github.com>
2023-08-20 02:54:18 -0400

    Time: 2 ms (73.33%) | Memory: 41 MB (91.45%) - LeetSync

--- 223-rectangle-area/rectangle-area.java
+++ 223-rectangle-area/rectangle-area.java
@@ -3,39 +3,19 @@ class Solution {
         int area_a = (ax2 - ax1) * (ay2 - ay1);
         int area_b = (bx2 - bx1) * (by2 - by1);
 
-        //check for total overlap
-        if (ax2 >= bx2 && ax1 <= bx1 && ay2 >= by2 && ay1 <= by1) {
-            return area_a;
-        }
-
-        if (bx2 >= ax2 && bx1 <= ax1 && by2 >= ay2 && by1 <= ay1) {
-            return area_b;
-        }
-
         //height overlap:
-        int heightOverlap = 0;
-        if (ay1 <= by1 && ay2 >= by2) {
-            heightOverlap = by2 - by1;
-        } else if (by1 <= ay1 && by2 >= ay2) {
-            heightOverlap = ay2 - ay1;
-        } else if (ay1 >= by1) {
-            heightOverlap = Math.max(0, by2 - ay1);
-        } else {
-            heightOverlap = Math.max(0, ay2 - by1);
-        }
+        int top = Math.min(ay2, by2);
+        int bottom = Math.max(ay1, by1);
 
         //width overlap:
-        int widthOverlap = 0;
-        if (ax1 <= bx1 && ax2 >= bx2) {
-            widthOverlap = bx2 - bx1;
-        } else if (bx1 <= ax1 && bx2 >= ax2) {
-            widthOverlap = ax2 - ax1;
-        } else if (ax1 >= bx1) {
-            widthOverlap = Math.max(0, bx2 - ax1);
-        } else {
-            widthOverlap = Math.max(0, ax2 - bx1);
+        int left = Math.max(ax1, bx1);
+        int right = Math.min(ax2, bx2);
+
+        int overlapArea = 0;
+        if (top - bottom > 0 && right - left > 0) {
+            overlapArea = (top - bottom) * (right - left);
         }
 
-        return area_a + area_b - (heightOverlap * widthOverlap);
+        return area_a + area_b - overlapArea;
     }
 }
