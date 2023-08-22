5864bf362fa55a89d10e33a15d9d2f10e73e70ce
cheny-414 <94153287+cheny-414@users.noreply.github.com>
2023-08-21 21:15:04 -0400

    Time: 2 ms (68.94%) | Memory: 44 MB (7.18%) - LeetSync

---
+++ 99-recover-binary-search-tree/recover-binary-search-tree.java
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    TreeNode x = null, y = null, pred = null;
+
+    public void swap(TreeNode a, TreeNode b) {
+        int tmp = a.val;
+        a.val = b.val;
+        b.val = tmp;
+    }
+
+    public void recoverTree(TreeNode root) {
+        Deque<TreeNode> stack = new ArrayDeque<>();
+        TreeNode curr = root;
+        while (curr != null || !stack.isEmpty()) {
+            if (curr != null) {
+                stack.push(curr);
+                curr = curr.left;
+            } else {
+                TreeNode node = stack.pop();
+                if (pred != null && node.val < pred.val) {
+                    y = node;
+                    if (x == null) x = pred;
+                    else break;
+                }
+                pred = node;
+                curr = node.right;
+            }
+        }
+        swap(x, y);
+    }
+}
