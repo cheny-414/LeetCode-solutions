1bc079011343ba9bb565d50484afb0023b54f62b
cheny-414 <94153287+cheny-414@users.noreply.github.com>
2023-08-21 20:44:59 -0400

    Time: 0 ms (100.00%) | Memory: 39.4 MB (36.10%) - LeetSync

--- 96-unique-binary-search-trees/unique-binary-search-trees.java
+++ 96-unique-binary-search-trees/unique-binary-search-trees.java
@@ -1,16 +1,19 @@
 class Solution {
+    int[][] dp;
     public int numTrees(int n) {
+        dp = new int[n + 1][n + 1];
         return arrangements(1, n);
     }
 
     private int arrangements(int start, int end) {
         if (start >= end) return 1;
+        if (dp[start][end] != 0) return dp[start][end];
         int answer = 0;
         for (int i = start; i <= end; i++) {
             int left = arrangements(start, i - 1);
             int right = arrangements(i + 1, end);
             answer += left * right;
         }
-        return answer;
+        return dp[start][end] = answer;
     }
 }
