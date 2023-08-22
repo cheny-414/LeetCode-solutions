99fa7f153643729bd60e543fbf4dfbbe50fb3bf8
cheny-414 <94153287+cheny-414@users.noreply.github.com>
2023-08-21 21:29:58 -0400

    Time: 6 ms (76.47%) | Memory: 43.3 MB (74.26%) - LeetSync

---
+++ 2738-count-the-number-of-k-free-subsets/count-the-number-of-k-free-subsets.java
@@ -0,0 +1,23 @@
+class Solution {
+    public long countTheNumOfKFreeSubsets(int[] nums, int k) {
+        Arrays.sort(nums);
+        Map<Integer, Integer> m = new HashMap<>();
+
+        for (int x: nums) {
+            int curr = m.getOrDefault(x - k, 0);
+            m.remove(x - k);
+            m.put(x, curr + 1);
+        }
+
+        long res = 1L;
+        for (int x: m.values()) res *= fib(x + 2);
+
+        return res;
+    }
+
+    public static long fib(int n) {
+        double phi = (1 + Math.sqrt(5)) / 2;
+        double f = (Math.pow(phi, n) - Math.pow(1 - phi, n)) / Math.sqrt(5);
+        return (long) Math.round(f);
+    }
+}
