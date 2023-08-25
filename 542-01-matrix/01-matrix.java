2549d6bb9ad257377d7840a2e6bfd0fc57ac2cad
cheny-414 <94153287+cheny-414@users.noreply.github.com>
2023-08-24 21:37:09 -0400

    Time: 18 ms (17.25%) | Memory: 45.5 MB (59.69%) - LeetSync

---
+++ 542-01-matrix/01-matrix.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int[][] updateMatrix(int[][] mat) {
+        int m = mat.length, n = mat[0].length;
+        int[][] answer = new int[m][n];
+        int[][] dirs = new int[][] {{0, 1}, {0, -1}, {-1, 0}, {1, 0}};
+        Queue<int[]> q = new ArrayDeque<>();
+        int distance = 0;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (mat[i][j] == 0) {
+                    q.add(new int[]{i, j});
+                }
+            }
+        }
+
+        while (!q.isEmpty()) {
+            int size = q.size();
+            for (int i = 0; i < size; i++) {
+                int[] curr = q.poll();
+                if (answer[curr[0]][curr[1]] > 0) continue;
+                answer[curr[0]][curr[1]] = distance;
+                mat[curr[0]][curr[1]] = 0;
+                for (int[] dir : dirs) {
+                    int nexti = curr[0] + dir[0];
+                    int nextj = curr[1] + dir[1];
+                    if (nexti >= 0 && nexti < m && nextj >= 0 && nextj < n && mat[nexti][nextj] == 1) {
+                        q.add(new int[]{nexti, nextj});
+                    }
+                }
+            }
+            distance++;
+        }
+        return answer;
+    }
+}
