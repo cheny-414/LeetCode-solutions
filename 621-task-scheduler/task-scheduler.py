d6f47dc4be7f243e43393652ec762039fd2575b3
cheny-414 <94153287+cheny-414@users.noreply.github.com>
2024-12-02 15:13:03 -0500

    Time: 15 ms (91.40%) | Memory: 19.1 MB (6.50%) - LeetSync

--- 621-task-scheduler/task-scheduler.py
+++ 621-task-scheduler/task-scheduler.py
@@ -3,9 +3,9 @@ class Solution:
         freq = [0] * 26
         for task in tasks:
             freq[ord(task) - ord('A')] += 1
-        freq.sort(reverse = False)
-        max_freq = freq[25] - 1
+        freq.sort(reverse = True)
+        max_freq = freq[0] - 1
         idle_slots = max_freq * n
-        for i in range (24, -1, -1):
+        for i in range (1, 26):
             idle_slots -= min(max_freq, freq[i])
         return max(len(tasks), len(tasks) + idle_slots)
