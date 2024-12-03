211a3f68770a2d17b205d7acbec71e3af99ac363
cheny-414 <94153287+cheny-414@users.noreply.github.com>
2024-12-02 20:20:38 -0500

    Time: 0 ms (100.00%) | Memory: 17.3 MB (5.58%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.py
+++ 88-merge-sorted-array/merge-sorted-array.py
@@ -1,22 +1,16 @@
-class Solution(object):
-    def merge(self, nums1, m, nums2, n):
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
         """
-        :type nums1: List[int]
-        :type m: int
-        :type nums2: List[int]
-        :type n: int
-        :rtype: None Do not return anything, modify nums1 in-place instead.
+        Do not return anything, modify nums1 in-place instead.
         """
-        nums1_copy = nums1[0:m]
+        p1 = m - 1
+        p2 = n - 1
 
-        p1 = 0
-        p2 = 0
-
-        for p in range(m + n):
-            if p2 >= n or (p1 < m and nums1_copy[p1] < nums2[p2]):
-                nums1[p] = nums1_copy[p1]
-                p1 += 1
+        for p3 in range(m + n - 1, -1, -1):
+            if p2 < 0: break
+            if p1 >= 0 and nums1[p1] > nums2[p2]:
+                nums1[p3] = nums1[p1]
+                p1 -= 1
             else:
-                nums1[p] = nums2[p2]
-                p2 += 1
-        
+                nums1[p3] = nums2[p2]
+                p2 -= 1
