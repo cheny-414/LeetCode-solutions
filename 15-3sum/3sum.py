af2fee01112217dc4417d7d25ae6b7956a52d017
cheny-414 <94153287+cheny-414@users.noreply.github.com>
2024-12-04 18:14:14 -0500

    Time: 551 ms (93.96%) | Memory: 18.1 MB (18.79%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -1,16 +1,20 @@
-class Solution:
+class Solution(object):
     def threeSum(self, nums):
-        res = []
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
         nums.sort()
+        result = []
         for i in range(len(nums)):
             if nums[i] > 0:
                 break
             if i == 0 or nums[i - 1] != nums[i]:
-                self.twoSumII(nums, i, res)
-        return res
-
-    def twoSumII(self, nums, i, res):
-        lo, hi = i + 1, len(nums) - 1
+                self.twoSum(nums, i, result)
+        return result
+    def twoSum(self, nums, i, res):
+        lo = i + 1
+        hi = len(nums) - 1
         while lo < hi:
             sum = nums[i] + nums[lo] + nums[hi]
             if sum < 0:
@@ -23,3 +27,4 @@ class Solution:
                 hi -= 1
                 while lo < hi and nums[lo] == nums[lo - 1]:
                     lo += 1
+
