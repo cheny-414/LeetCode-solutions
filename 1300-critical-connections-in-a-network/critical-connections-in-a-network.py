ec25ea33eed69515b18de2949fc687af67215609
cheny-414 <94153287+cheny-414@users.noreply.github.com>
2024-12-06 13:18:06 -0500

    Time: 593 ms (5.88%) | Memory: 85.8 MB (15.64%) - LeetSync

--- 1300-critical-connections-in-a-network/critical-connections-in-a-network.py
+++ 1300-critical-connections-in-a-network/critical-connections-in-a-network.py
@@ -2,7 +2,7 @@ class Solution:
     def criticalConnections(self, n: int, connections: List[List[int]]) -> List[List[int]]:
         rank = {}
         graph = defaultdict(list)
-        conn_dict = set()
+        conn_set = set()
 
         for i in range(n):
             rank[i] = None
@@ -11,7 +11,7 @@ class Solution:
             u, v = edge[0], edge[1]
             graph[u].append(v)
             graph[v].append(u)
-            conn_dict.add((min(u, v), max(u, v)))
+            conn_set.add((min(u, v), max(u, v)))
 
         def dfs(node, discovery_rank):
             if rank[node]:
@@ -24,9 +24,9 @@ class Solution:
                     continue
                 recursive_rank = dfs(neighbor, discovery_rank + 1)
                 if recursive_rank <= discovery_rank:
-                    conn_dict.remove((min(node, neighbor), max(node, neighbor)))
+                    conn_set.remove((min(node, neighbor), max(node, neighbor)))
                 min_rank = min(min_rank, recursive_rank)
             return min_rank
 
         dfs(0, 0)
-        return list(conn_dict)
+        return list(conn_set)
