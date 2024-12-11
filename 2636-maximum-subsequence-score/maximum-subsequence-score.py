4b7199e3c540317c34d99ca106034867cc01b3c9
cheny-414 <94153287+cheny-414@users.noreply.github.com>
2024-12-11 18:03:20 -0500

    Time: 207 ms (22.19%) | Memory: 42.4 MB (10.81%) - LeetSync

--- 2636-maximum-subsequence-score/maximum-subsequence-score.py
+++ 2636-maximum-subsequence-score/maximum-subsequence-score.py
@@ -1,27 +1,19 @@
 class Solution:
     def maxScore(self, nums1: List[int], nums2: List[int], k: int) -> int:
-        # Sort pair (nums1[i], nums2[i]) by nums2[i] in decreasing order.
-        pairs = [(a, b) for a, b in zip(nums1, nums2)]
-        pairs.sort(key = lambda x: -x[1])
-        
-        # Use a min-heap to maintain the top k elements.
-        top_k_heap = [x[0] for x in pairs[:k]]
-        top_k_sum = sum(top_k_heap)
-        heapq.heapify(top_k_heap)
-        
-        # The score of the first k pairs.
-        answer = top_k_sum * pairs[k - 1][1]
-        
-        # Iterate over every nums2[i] as minimum from nums2.
-        for i in range(k, len(nums1)):
-            # Remove the smallest integer from the previous top k elements
-            # then add nums1[i] to the top k elements.
-            top_k_sum -= heapq.heappop(top_k_heap)
-            top_k_sum += pairs[i][0]
-            heapq.heappush(top_k_heap, pairs[i][0])
-            
-            # Update answer as the maximum score.
-            answer = max(answer, top_k_sum * pairs[i][1])
-        
-        return answer
-            
+        pairs = []
+        for i in range(len(nums1)):
+            val1, val2 = nums1[i], nums2[i]
+            pairs.append((-val2, val1))
+        pairs.sort()
+        k_heap = [x[1] for x in pairs[:k]]
+        heapify(k_heap)
+        k_sum = sum(k_heap)
+        result = k_sum * (-pairs[k - 1][0])
+        for i in range(k, len(pairs)):
+            height = -pairs[i][0]
+            k_sum -= heappop(k_heap)
+            heappush(k_heap, pairs[i][1])
+            k_sum += pairs[i][1]
+            result = max(result, height * k_sum)
+        return result
+
