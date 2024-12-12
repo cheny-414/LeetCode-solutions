d9ee45c620a33670f79b244140d1d894718977e4
cheny-414 <94153287+cheny-414@users.noreply.github.com>
2024-12-12 16:00:05 -0500

    Time: 2 ms (90.02%) | Memory: 18.4 MB (37.79%) - LeetSync

--- 120-triangle/triangle.py
+++ 120-triangle/triangle.py
@@ -2,16 +2,16 @@ class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
         n = len(triangle[-1])
         m = len(triangle)
-        dp = [[float('inf')] * n for _ in range(m)]
+        #dp = [[float('inf')] * n for _ in range(m)]
         dp_alt = [float('inf')] * n
 
         for i in range(n):
             #dp[m - 1][i] = triangle[m - 1][i]
             dp_alt[i] = triangle[m - 1][i]
         for i in range(m - 2, -1, -1):
-            new_dp = [0] * n
+            #new_dp = [0] * n
             for j in range(n - (m - i - 1)):
                 #dp[i][j] = min(dp[i + 1][j], dp[i + 1][j + 1]) + triangle[i][j]
-                new_dp[j] = min(dp_alt[j], dp_alt[j + 1]) + triangle[i][j]
-            dp_alt = new_dp
+                dp_alt[j] = min(dp_alt[j], dp_alt[j + 1]) + triangle[i][j]
+            #dp_alt = new_dp
         return dp_alt[0]
