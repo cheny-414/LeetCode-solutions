fab0b5a747da795936bff9bdbd3f361106d364a7
cheny-414 <94153287+cheny-414@users.noreply.github.com>
2024-12-11 22:01:24 -0500

    Time: 66 ms (56.22%) | Memory: 20.8 MB (14.16%) - LeetSync

--- 72-edit-distance/edit-distance.py
+++ 72-edit-distance/edit-distance.py
@@ -1,23 +1,14 @@
-class Solution(object):
-    def minDistance(self, word1, word2):
-        """
-        :type word1: str
-        :type word2: str
-        :rtype: int
-        """
-        dp = [[0 for _ in range(len(word2) + 1)] for _ in range(len(word1) + 1)]
-
-
-        for i in range(1, len(word1) + 1):
+class Solution:
+    def minDistance(self, word1: str, word2: str) -> int:
+        dp = [[-1] * (len(word2) + 1) for _ in range(len(word1) + 1)]
+        for i in range(len(word1) + 1):
             dp[i][0] = i
-        for i in range(1, len(word2) + 1):
+        for i in range(len(word2) + 1):
             dp[0][i] = i
-        for i in range(1, len(word1) + 1):
-            for j in range(1, len(word2) + 1):
-                if word2[j - 1] == word1[i - 1]:
-                    dp[i][j] = dp[i - 1][j - 1]
+        for i in range(len(word1)):
+            for j in range(len(word2)):
+                if word1[i] == word2[j]:
+                    dp[i + 1][j + 1] = dp[i][j]
                 else:
-                    dp[i][j] = min(dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1]) + 1
+                    dp[i + 1][j + 1] = min(dp[i][j + 1], dp[i + 1][j], dp[i][j]) + 1
         return dp[len(word1)][len(word2)]
-
-        
