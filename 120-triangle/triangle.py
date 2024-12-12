1be68132bdeacd814504c8bb30bf3ba92eb20731
cheny-414 <94153287+cheny-414@users.noreply.github.com>
2024-12-12 15:58:36 -0500

    Time: 3 ms (85.96%) | Memory: 18.3 MB (45.09%) - LeetSync

--- 120-triangle/triangle.py
+++ 120-triangle/triangle.py
@@ -3,10 +3,15 @@ class Solution:
         n = len(triangle[-1])
         m = len(triangle)
         dp = [[float('inf')] * n for _ in range(m)]
+        dp_alt = [float('inf')] * n
 
         for i in range(n):
-            dp[m - 1][i] = triangle[m - 1][i]
+            #dp[m - 1][i] = triangle[m - 1][i]
+            dp_alt[i] = triangle[m - 1][i]
         for i in range(m - 2, -1, -1):
+            new_dp = [0] * n
             for j in range(n - (m - i - 1)):
-                dp[i][j] = min(dp[i + 1][j], dp[i + 1][j + 1]) + triangle[i][j]
-        return dp[0][0]
+                #dp[i][j] = min(dp[i + 1][j], dp[i + 1][j + 1]) + triangle[i][j]
+                new_dp[j] = min(dp_alt[j], dp_alt[j + 1]) + triangle[i][j]
+            dp_alt = new_dp
+        return dp_alt[0]
