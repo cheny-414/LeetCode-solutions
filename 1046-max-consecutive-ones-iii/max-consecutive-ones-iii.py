0a35f4848a58b589d9925fcdde754ba025924450
cheny-414 <94153287+cheny-414@users.noreply.github.com>
2024-12-12 22:45:54 -0500

    Time: 71 ms (39.45%) | Memory: 17.7 MB (18.32%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.py
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.py
@@ -3,12 +3,13 @@ class Solution:
         left = 0
         zeroes = 0
         longest = 0
+        best = 0
         for right in range(len(nums)):
-            size = right - left + 1
             if nums[right] == 0:
                 zeroes += 1
             if zeroes > k:
                 if nums[left] == 0:
                     zeroes -= 1
                 left += 1
-        return right - left + 1
+            best = max(best, right - left + 1)
+        return best
