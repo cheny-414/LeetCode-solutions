5dbbdb89fd7abbe8fc02eccff1d15351355b8c14
cheny-414 <94153287+cheny-414@users.noreply.github.com>
2024-12-13 16:01:33 -0500

    Time: 97 ms (61.22%) | Memory: 21.1 MB (29.11%) - LeetSync

--- 954-maximum-sum-circular-subarray/maximum-sum-circular-subarray.py
+++ 954-maximum-sum-circular-subarray/maximum-sum-circular-subarray.py
@@ -2,15 +2,15 @@ class Solution:
     def maxSubarraySumCircular(self, nums: List[int]) -> int:
         currMax = 0
         currMin = 0
-        bestMax = nums[0]
-        bestMin = nums[0]
+        bestMaxSum = nums[0]
+        bestMinSum = nums[0]
         total = 0
         for num in nums:
             currMax = max(currMax + num, num)
             currMin = min(currMin + num, num)
-            bestMax = max(bestMax, currMax)
-            bestMin = min(bestMin, currMin)
+            bestMaxSum = max(bestMaxSum, currMax)
+            bestMinSum = min(bestMinSum, currMin)
             total += num
-        if total == bestMin:
-            return bestMax
-        return max(bestMax, total - bestMin)
+        if total == bestMinSum:
+            return bestMaxSum
+        return max(bestMaxSum, total - bestMinSum)
