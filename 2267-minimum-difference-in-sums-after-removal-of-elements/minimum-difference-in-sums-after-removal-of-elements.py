4dbbd8323b6751597e9ef3e79b03faa9ccd4e76b
cheny-414 <94153287+cheny-414@users.noreply.github.com>
2024-12-13 00:18:00 -0500

    Time: 417 ms (53.64%) | Memory: 51.1 MB (18.33%) - LeetSync

--- 2267-minimum-difference-in-sums-after-removal-of-elements/minimum-difference-in-sums-after-removal-of-elements.py
+++ 2267-minimum-difference-in-sums-after-removal-of-elements/minimum-difference-in-sums-after-removal-of-elements.py
@@ -2,40 +2,29 @@ class Solution:
     def minimumDifference(self, nums: List[int]) -> int:
         n = len(nums) // 3
 
-        # calculate max_sum using min_heap for second part
-        min_heap = nums[(2 * n) :]
-        heapq.heapify(min_heap)
-
-        max_sum = [0] * (n + 2)
-        max_sum[n + 1] = sum(min_heap)
-        for i in range((2 * n) - 1, n - 1, -1):
-            # push current
-            heapq.heappush(min_heap, nums[i])
-            # popout minimum from heap
-            val = heapq.heappop(min_heap)
-            # max_sum for this partition
-            max_sum[i - n + 1] = max_sum[i - n + 2] - val + nums[i]
-
-
-        # calculate min_sum using max_heap for first part
         max_heap = [-x for x in nums[:n]]
-        heapq.heapify(max_heap)
-
-        min_sum = [0] * (n + 2)
+        heapify(max_heap)
+        min_sum = [0] * (n + 1)
         min_sum[0] = -sum(max_heap)
-        for i in range(n, (2 * n)):
-            # push current
-            heapq.heappush(max_heap, -nums[i])
-            # popout maximum from heap
-            val = -heapq.heappop(max_heap)
-            # min_sum for this partition
+
+        for i in range(n, 2 * n):
+            heappush(max_heap, -nums[i])
+            val = -heappop(max_heap)
             min_sum[i - n + 1] = min_sum[i - n] - val + nums[i]
 
+        min_heap = nums[2*n:]
+        heapify(min_heap)
+        max_sum = [0] * (n + 1)
+        max_sum[n] = sum(min_heap)
 
-        # find min difference bw second part (max_sum) and first part (min_sum)
-        ans = math.inf
+        best = float('inf')
+        for i in range(2 * n - 1, n - 1, -1):
+            heappush(min_heap, nums[i])
+            val = heappop(min_heap)
+            max_sum[i - n] = max_sum[i - n + 1] - val + nums[i]
+
         for i in range(0, n + 1):
-            print(i, min_sum[i], max_sum[i])
-            ans = min((min_sum[i] - max_sum[i + 1]), ans)
-
-        return ans
+            left = min_sum[i]
+            right = max_sum[i]
+            best = min(best, left - right)
+        return best
