33c7809e25bfd2fe1a77bc9240ad3987fe044e10
cheny-414 <94153287+cheny-414@users.noreply.github.com>
2024-12-18 01:41:11 -0500

    Time: 127 ms (72.55%) | Memory: 78.7 MB (6.16%) - LeetSync

--- 146-lru-cache/lru-cache.py
+++ 146-lru-cache/lru-cache.py
@@ -1,50 +1,51 @@
 class Node:
-    def __init__(self, key, value):
+    def __init__(self, key, val: int):
         self.key = key
-        self.value = value
+        self.val = val
         self.prev = None
         self.next = None
-
 class LRUCache:
 
     def __init__(self, capacity: int):
-        self.cache = dict()
-        self.capacity = capacity
+        self.cache = {}
         self.head = Node(-1, -1)
-        self.tail = Node (-1, -1)
+        self.tail = Node(-1, -1)
         self.head.next = self.tail
         self.tail.prev = self.head
-
+        self.capacity = capacity
 
     def get(self, key: int) -> int:
-        if key in self.cache:
-            node = self.cache[key]
-            self.remove(node)
-            self.add(node)
-            return node.value
-        return -1
+        if key not in self.cache:
+            return -1
+        node = self.cache[key]
+        self.remove(node)
+        self.add(node)
+        return node.val
+        
 
     def put(self, key: int, value: int) -> None:
         if key in self.cache:
-            self.remove(self.cache[key])
-        elif len(self.cache) == self.capacity:
-            node_to_delete = self.head.next
-            self.remove(node_to_delete)
-            self.cache.pop(node_to_delete.key)
-
-        node = Node(key, value)
-        self.cache[key] = node
-        self.add(node)
-
-    def remove(self, node):
-        node.prev.next = node.next
-        node.next.prev = node.prev
-    
+            node = self.cache[key]
+            self.remove(node)
+        nodeToAdd = Node(key, value)
+        self.add(nodeToAdd)
+        self.cache[key] = nodeToAdd
+        if len(self.cache) > self.capacity:
+            nodeToRemove = self.head.next
+            self.remove(nodeToRemove)
+            del self.cache[nodeToRemove.key]
     def add(self, node):
-        node.prev = self.tail.prev
-        self.tail.prev.next = node
+        lastNode = self.tail.prev
+        lastNode.next = node
+        node.prev = lastNode
         node.next = self.tail
         self.tail.prev = node
+    
+    def remove(self, node):
+        prevNode = node.prev
+        nextNode = node.next
+        prevNode.next = nextNode
+        nextNode.prev = prevNode
 
 
 # Your LRUCache object will be instantiated and called as such:
