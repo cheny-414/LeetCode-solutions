a76740fee3cbf7952dd2985a43feb43591b301c4
cheny-414 <94153287+cheny-414@users.noreply.github.com>
2024-12-19 13:16:53 -0500

    Time: 128 ms (30.44%) | Memory: 28.4 MB (32.70%) - LeetSync

---
+++ 1325-path-with-maximum-probability/path-with-maximum-probability.py
@@ -0,0 +1,20 @@
+class Solution:
+    def maxProbability(self, n: int, edges: List[List[int]], succProb: List[float], start_node: int, end_node: int) -> float:
+        pq = [(-1, start_node)]
+        probs = [0] * n
+        probs[start_node] = 1
+        adj = defaultdict(list)
+        #visited = [False] * n
+        for i, edge in enumerate(edges):
+            adj[edge[0]].append((succProb[i], edge[1]))
+            adj[edge[1]].append((succProb[i], edge[0]))
+        while pq:
+            prob, node = heappop(pq)
+            # if node == end_node:
+            #     return prob
+            for nextProb, neighbor in adj[node]:
+                overallProb = -prob * nextProb
+                if probs[neighbor] < overallProb:
+                    probs[neighbor] = overallProb
+                    heappush(pq, (-overallProb, neighbor))
+        return probs[end_node]
