f0ebd2868738bda4c004f32186d2378d6b9682e9
cheny-414 <94153287+cheny-414@users.noreply.github.com>
2024-12-19 14:12:17 -0500

    Time: 386 ms (33.69%) | Memory: 19.8 MB (5.32%) - LeetSync

---
+++ 744-network-delay-time/network-delay-time.py
@@ -0,0 +1,15 @@
+class Solution:
+    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        delays = [float('inf')] * n
+        delays[k - 1] = 0
+        pq = [(0, k - 1)]
+        adj = defaultdict(list)
+        for u, v, w in times:
+            adj[u - 1].append((w, v - 1))
+        while pq:
+            delay, node = heappop(pq)
+            for w, v in adj[node]:
+                if delays[v] > w + delay:
+                    delays[v] = w + delay
+                    heappush(pq, (delays[v], v))
+        return -1 if max(delays) == float('inf') else max(delays)
