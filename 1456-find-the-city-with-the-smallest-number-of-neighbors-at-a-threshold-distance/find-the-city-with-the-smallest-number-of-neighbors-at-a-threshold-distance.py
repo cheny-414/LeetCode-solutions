31e55d232ed9c2b4ef87eb57db802d94213fdfc9
cheny-414 <94153287+cheny-414@users.noreply.github.com>
2024-12-19 21:13:13 -0500

    Time: 2858 ms (5.05%) | Memory: 18.4 MB (43.09%) - LeetSync

--- 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.py
+++ 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.py
@@ -1,20 +1,24 @@
 class Solution:
     def findTheCity(self, n: int, edges: List[List[int]], distanceThreshold: int) -> int:
-        dist = [[float('inf')] * n for _ in range(n)]
-        for i in range(n):
-            dist[i][i] = 0
-        for edge in edges:
-            dist[edge[0]][edge[1]] = edge[2]
-            dist[edge[1]][edge[0]] = edge[2]
-        for k in range(n):
-            for i in range(n):
-                for j in  range(n):
-                    dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j])
-        smallestCity = -1
+        res = []
+        def bellmanFord(start):
+            dist = [float('inf')] * n
+            dist[start] = 0
+
+            for k in range(n - 1):
+                for source, dest, weight in edges:
+                    if dist[source] + weight < dist[dest]:
+                        dist[dest] = dist[source] + weight
+                    if dist[dest] + weight < dist[source]:
+                        dist[source] = dist[dest] + weight
+            
+            return sum(1 for x in dist if x <= distanceThreshold)
+        
+        smallest = -1
         smallestCount = float('inf')
         for i in range(n):
-            count = sum(1 for x in dist[i] if x <= distanceThreshold)
+            count = bellmanFord(i)
             if count <= smallestCount:
-                smallestCity = i
+                smallest = i
                 smallestCount = count
-        return smallestCity
+        return smallest
