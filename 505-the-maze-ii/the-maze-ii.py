af2c6686fceef7f3f44569dfeb4ab196f9c946f8
cheny-414 <94153287+cheny-414@users.noreply.github.com>
2024-12-19 19:18:43 -0500

    Time: 62 ms (33.38%) | Memory: 19.1 MB (5.59%) - LeetSync

--- 505-the-maze-ii/the-maze-ii.py
+++ 505-the-maze-ii/the-maze-ii.py
@@ -1,9 +1,5 @@
 class Solution:
     def shortestDistance(self, maze: List[List[int]], start: List[int], destination: List[int]) -> int:
-        #0, 4, left
-        #0, 4, down
-
-        #0, 3, right/down
         m, n = len(maze), len(maze[0])
         dir = ((0, 1), (1, 0), (0, -1), (-1, 0))
         visited = [[[False] * 4 for _ in range(n)] for _ in range(m)]
@@ -18,17 +14,19 @@ class Solution:
             size = len(q)
             for k in range(size):
                 i, j, num = q.popleft()
-                #append neighbors
+                #check if we are about to hit the destination
                 neii, neij = i + dir[num][0], j + dir[num][1]
                 nexti, nextj = neii + dir[num][0], neij + dir[num][1]
                 if 0 <= neii < m and 0 <= neij < n and neii == destination[0] and neij == destination[1]:
                     if nexti < 0 or nexti >= m or nextj < 0 or nextj >= n or maze[nexti][nextj] == 1:
                         return steps + 1
-                #only one direction choosable
+                #append neighbors
                 if 0 <= neii < m and 0 <= neij < n and maze[neii][neij] == 0:
+                    #only one direction choosable
                     if 0 <= nexti < m and 0 <= nextj < n and maze[nexti][nextj] == 0 and not visited[neii][neij][num]:
                         q.append((neii, neij, num))
                         visited[neii][neij][num] = True
+                    #choose any available direction
                     else:
                         for order, direction in enumerate(dir):
                             if 0 <= neii + direction[0] < m and 0 <= neij + direction[1] < n and not visited[neii][neij][order] and maze[neii + direction[0]][neij + direction[1]] == 0:
