db041309ac17333ca41029160a0da60110130b9a
cheny-414 <94153287+cheny-414@users.noreply.github.com>
2024-12-22 18:55:15 -0500

    Time: 527 ms (40.00%) | Memory: 17.7 MB (6.67%) - LeetSync

--- 668-kth-smallest-number-in-multiplication-table/kth-smallest-number-in-multiplication-table.py
+++ 668-kth-smallest-number-in-multiplication-table/kth-smallest-number-in-multiplication-table.py
@@ -2,19 +2,8 @@ class Solution:
     def findKthNumber(self, m: int, n: int, k: int) -> int:
         def num_of_numbers_lte_x(x):
             res = 0
-            for i in range(m):
-                cand = 0
-                num1 = i + 1
-                left = 1
-                right = n
-                while left <= right:
-                    num2 = left + (right - left) // 2
-                    if num1 * num2 <= x:
-                        cand = num2
-                        left = num2 + 1
-                    else:
-                        right = num2 - 1
-                res += cand
+            for i in range(1, (m + 1)):
+                res += min(x // i, n)
             return res
         print(num_of_numbers_lte_x(2))
         left = 1
