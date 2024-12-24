c24b2fc22fbe91010ae619d716df7309b38e09e6
cheny-414 <94153287+cheny-414@users.noreply.github.com>
2024-12-23 22:39:08 -0500

    Time: 0 ms (100.00%) | Memory: 18 MB (5.58%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.py
+++ 54-spiral-matrix/spiral-matrix.py
@@ -9,9 +9,9 @@ class Solution:
         while 0 <= curr[0] < m and 0 <= curr[1] < n and matrix[curr[0]][curr[1]] != -101:
             res.append(matrix[curr[0]][curr[1]])
             matrix[curr[0]][curr[1]]  = -101
-            nexti = curr[0] + dirs[currDir % 4][0]
-            nextj = curr[1] + dirs[currDir % 4][1]
+            nexti = curr[0] + dirs[currDir][0]
+            nextj = curr[1] + dirs[currDir][1]
             if nexti < 0 or nexti > m - 1 or nextj < 0 or nextj > n - 1 or matrix[nexti][nextj] == -101:
-                currDir += 1
-            curr = (curr[0] + dirs[currDir % 4][0], curr[1] + dirs[currDir % 4][1])
+                currDir = (currDir + 1) % 4
+            curr = (curr[0] + dirs[currDir][0], curr[1] + dirs[currDir][1])
         return res
