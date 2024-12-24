69ba6feb95ec7005e1bcef491fa84d6620f4b2c4
cheny-414 <94153287+cheny-414@users.noreply.github.com>
2024-12-23 19:30:47 -0500

    Time: 22 ms (79.45%) | Memory: 18.1 MB (8.01%) - LeetSync

--- 3644-minimum-positive-sum-subarray/minimum-positive-sum-subarray.py
+++ 3644-minimum-positive-sum-subarray/minimum-positive-sum-subarray.py
@@ -1,19 +1,21 @@
 class Solution:
     def minimumSumSubarray(self, nums: List[int], l: int, r: int) -> int:
-        # prefix_sum = [0] * len(nums)
-        # for i, num in enumerate(nums):
-        #     prev = 0 if i == 0 else prefix_sum[i - 1]
-        #     prefix_sum[i] = prev + num
+        #0, 1, 2, 3
+        #1, 2, 3, 4
+        #1, 3, 6, 10
+        prefix_sum = [0] * len(nums)
+        for i, num in enumerate(nums):
+            prev = 0 if i == 0 else prefix_sum[i - 1]
+            prefix_sum[i] = prev + num
         def findMin(length):
             minSum = float('inf')
-            running = 0
-            for i in range(length):
-                running += nums[i]
-            if running > 0:
-                minSum = running
-            for i in range(length, len(nums)):
-                running -= nums[i - length]
-                running += nums[i]
+            # running = 0
+            # for i in range(length):
+            #     running += nums[i]
+            # if running > 0:
+            #     minSum = running
+            for i in range(length - 1, len(nums)):
+                running = prefix_sum[i] - (0 if i == length - 1 else prefix_sum[i - length])
                 if running > 0:
                     minSum = min(minSum, running)
             return minSum
