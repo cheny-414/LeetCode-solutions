a4506fae5d5e778951089b11ffb09d602ce39f67
cheny-414 <94153287+cheny-414@users.noreply.github.com>
2024-12-26 12:46:14 -0500

    Time: 7 ms (56.72%) | Memory: 18.8 MB (5.06%) - LeetSync

--- 1028-interval-list-intersections/interval-list-intersections.py
+++ 1028-interval-list-intersections/interval-list-intersections.py
@@ -5,10 +5,6 @@ class Solution:
         m, n = len(firstList), len(secondList)
         if not firstList or not secondList:
             return res
-        # while i < m and j < n and firstList[i][1] < secondList[j][0]:
-        #     i += 1
-        # while i < m and j < n and secondList[j][1] < firstList[i][0]:
-        #     j += 1
         while i < m and j < n:
             if firstList[i][1] < secondList[j][0]:
                 i += 1
@@ -19,9 +15,6 @@ class Solution:
                 res.append(currIntersection)
                 if firstList[i][1] < secondList[j][1]:
                     i += 1
-                elif firstList[i][1] > secondList[j][1]:
-                    j += 1
                 else:
-                    i += 1
                     j += 1
         return res
