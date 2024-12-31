1f1f647974418e69739bdd95464e8f9b377f2a11
cheny-414 <94153287+cheny-414@users.noreply.github.com>
2024-12-30 21:26:06 -0500

    Time: 180 ms (47.24%) | Memory: 77.6 MB (52.76%) - LeetSync

---
+++ 2562-count-ways-to-build-good-strings/count-ways-to-build-good-strings.py
@@ -0,0 +1,21 @@
+class Solution:
+    def countGoodStrings(self, low: int, high: int, zero: int, one: int) -> int:
+        dp = {}
+        def helper(index):
+            if index > high - 1:
+                return 0
+            if index == high - 1:
+                return 1
+            if index in dp:
+                return dp[index]
+            ways = 0
+            if index >= low - 1:
+                #not append anything
+                ways += 1
+            #append zero
+            ways += helper(index + zero)
+            #append one
+            ways += helper(index + one)
+            dp[index] = ways % (10**9 + 7)
+            return dp[index]
+        return helper(-1) % (10**9 + 7)
