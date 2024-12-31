bef0f8c28e86f6584aec0bf0e790ffef1065b56e
cheny-414 <94153287+cheny-414@users.noreply.github.com>
2024-12-31 00:29:01 -0500

    Time: 99 ms (54.23%) | Memory: 18.1 MB (39.31%) - LeetSync

--- 962-flip-string-to-monotone-increasing/flip-string-to-monotone-increasing.py
+++ 962-flip-string-to-monotone-increasing/flip-string-to-monotone-increasing.py
@@ -1,18 +1,12 @@
 class Solution:
     def minFlipsMonoIncr(self, s: str) -> int:
-        dp = [[-1] * 2 for _ in range(len(s))]
-        def helper(index, requireOne):
-            if index >= len(s):
-                return 0
-            if dp[index][requireOne] != -1:
-                return dp[index][requireOne]
-            flips = inf
-            if not requireOne:
-                #flip to zero
-                flips = min(flips, (1 if s[index] == '1' else 0) + helper(index + 1, 0))
-            #flip to one
-            flips = min(flips, (1 if s[index] == '0' else 0) + helper(index + 1, 1))
-            dp[index][requireOne] = flips
-            return flips
-        return helper(0, 0)
-            
+        counter = Counter(s)
+        currentFlips = counter.get('0', 0)
+        answer = currentFlips
+        for split in range(len(s)):
+            if s[split] == '0':
+                currentFlips -= 1
+                answer = min(answer, currentFlips)
+            else:
+                currentFlips += 1
+        return answer
