58dda0678ef08a3697929fdde36c0587936e5870
cheny-414 <94153287+cheny-414@users.noreply.github.com>
2025-01-02 18:52:57 -0500

    Time: 3768 ms (9.03%) | Memory: 794 MB (5.48%) - LeetSync

---
+++ 313-super-ugly-number/super-ugly-number.py
@@ -0,0 +1,12 @@
+class Solution:
+    def nthSuperUglyNumber(self, n: int, primes: List[int]) -> int:
+        nums = primes.copy()
+        numsSet = set(nums)
+        p = 1
+        for i in range(n - 1):
+            p = heappop(nums)
+            for prime in primes:
+                if p * prime not in numsSet:
+                    heappush(nums, p * prime)
+                    numsSet.add(p * prime)
+        return p
