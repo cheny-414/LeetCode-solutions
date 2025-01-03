9969a4b16f15b2a8e7da442f792db047c1c87c00
cheny-414 <94153287+cheny-414@users.noreply.github.com>
2025-01-02 21:59:13 -0500

    Time: 0 ms (100.00%) | Memory: 17.6 MB (26.44%) - LeetSync

--- 119-pascals-triangle-ii/pascals-triangle-ii.py
+++ 119-pascals-triangle-ii/pascals-triangle-ii.py
@@ -1,9 +1,5 @@
 class Solution:
     def getRow(self, rowIndex: int) -> List[int]:
-        if rowIndex == 0:
-            return [1]
-        if rowIndex == 1:
-            return [1, 1]
         res = [1]
         curr = 1
         for i in range(1, rowIndex + 1):
