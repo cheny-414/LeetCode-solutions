5c1f787f8fd88340a3162bdaf763d008657d0e83
cheny-414 <94153287+cheny-414@users.noreply.github.com>
2025-01-07 14:07:34 -0500

    Time: 10 ms (58.66%) | Memory: 20.8 MB (5.49%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.py
+++ 152-maximum-product-subarray/maximum-product-subarray.py
@@ -5,22 +5,22 @@ class Solution:
         n = len(nums)
         if len(nums) == 1:
             return nums[0]
-        largest = [[inf, -inf] for _ in range(n)] #best negative, best positive
+        largest = [] #best negative, best positive
         if nums[0] < 0:
-            largest[0][0] = nums[0]
+            largest.append((nums[0], -inf))
         elif nums[0] > 0:
-            largest[0][1] = nums[0]
+            largest.append((inf, nums[0]))
         else:
-            largest[0] = [0, 0]
+            largest.append((0, 0))
         for i in range(1, n):
             if nums[i] == 0:
-                largest[i] = [0, 0]
+                largest.append((0, 0))
             elif nums[i] > 0:
                 negative = largest[i - 1][0] * nums[i]
                 positive = max(nums[i], largest[i - 1][1] * nums[i])
-                largest[i] = [negative, positive]
+                largest.append((negative, positive))
             else:
                 negative = min(nums[i], largest[i - 1][1] * nums[i])
                 positive = largest[i - 1][0] * nums[i]
-                largest[i] = [negative, positive]
+                largest.append((negative, positive))
         return max([x[1] for x in largest])
