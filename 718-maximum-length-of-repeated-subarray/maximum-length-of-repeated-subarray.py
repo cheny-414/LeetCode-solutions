5756c99dd1b251d477a0e12f20c2ef4f1b26718e
cheny-414 <94153287+cheny-414@users.noreply.github.com>
2025-01-08 00:38:54 -0500

    Time: 856 ms (82.05%) | Memory: 135.3 MB (5.55%) - LeetSync

--- 718-maximum-length-of-repeated-subarray/maximum-length-of-repeated-subarray.py
+++ 718-maximum-length-of-repeated-subarray/maximum-length-of-repeated-subarray.py
@@ -1,6 +1,6 @@
 class Solution:
     def findLength(self, nums1: List[int], nums2: List[int]) -> int:
-        cache = [[-1] * (len(nums2) + 1) for _ in range(len(nums1) + 1)]
+        cache = [[0] * (len(nums2) + 1) for _ in range(len(nums1) + 1)]
         def longest(p1, p2):
             if p1 == len(nums1) or p2 == len(nums2):
                 return 0
@@ -13,6 +13,10 @@ class Solution:
             longest(p1, p2 + 1)
             longest(p1 + 1, p2)
             return cache[p1][p2]
-        longest(0, 0)
+        #longest(0, 0)
+        for i in range(len(nums1)):
+            for j in range(len(nums2)):
+                if nums1[i] == nums2[j]:
+                    cache[i][j] = cache[i - 1][j - 1] + 1
         return max(max(row) for row in cache)
                 
